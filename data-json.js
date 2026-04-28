ecf5f80d89aab835f7f8be332d5f5370002b7965
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-04-28 09:16:38 +0600

    Update and rename json.js to data-json.js

---
+++ data-json.js
@@ -0,0 +1,27 @@
+{
+      "Football": {
+        "logo": "https://marketplace.canva.com/EAFItL26pj4/2/0/1600w/canva-green-blue-white-simple-photo-football-highlights-youtube-thumbnail-8l0_m5zVuqA.jpg",
+        "category": "Pin",
+        "title": "ARG VS BRA",
+        "time-date": "09:20 AM | 27-04-2026",
+        "status": "",
+        "servers": [
+          { "name": "Server 1", "url": "https://live-test.tsports.com/live-01.m3u8" },
+          { "name": "Server 2", "url": "https://live-test.tsports.com/live-02.m3u8" }
+        ]
+      },
+      "Somoy TV": {
+        "logo": "https://example.com/somoytv-logo.png",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1702/output/index.m3u8" }
+        ]
+      },
+      "Independent TV": {
+        "logo": "https://example.com/independenttv-logo.png",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1704/output/index.m3u8" }
+        ]
+      }
+    }
