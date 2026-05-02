6a23698466c22077cfea48f7dba52ec486b5978c
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-02 17:48:14 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -10,6 +10,14 @@
           { "name": "Server 2", "url": "https://live-test.tsports.com/live-02.m3u8" }
         ]
       },
+
+      "Bangla Waz": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Waz-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://live20.bozztv.com/giatvplayout7/giatv-209617/tracks-v1a1/mono.ts.m3u8" }
+        ]
+      },
       
       "ATN News": {
         "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/ATNNews-logo.webp",
@@ -47,8 +55,222 @@
         "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Channel9-logo.webp",
         "category": "Bangla",
         "servers": [
-          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/channel9hd.stream/tracks-v1a1/mono.m3u8" },
-          { "name": "Server 2", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1729/output/index.m3u8" }
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1729/output/index.m3u8" },
+          { "name": "Server 2", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/channel9hd.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Channel 24": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Channel24-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1703/output/index.m3u8" }
+        ]
+      },
+
+      "Channel i": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Channeli-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1723/output/index.m3u8" },
+          { "name": "Server 2", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/channeli-8-org.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Channel S": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/ChannelS-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app.ncare.live/live-orgin/channels.stream/live-orgin/channels.stream/chunks.m3u8?nimblesessionid=1033225798" }
+        ]
+      },
+
+      "DBC News": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/DBCNews-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1728/output/index.m3u8" },
+          { "name": "Server 2", "url": "https://bozztv.com/rongo/rongo-dbcnews/tracks-v2a1/mono.m3u8" }
+        ]
+      },
+
+      "Deepto TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/DeeptoTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1711/output/index.m3u8" },
+          { "name": "Server 2", "url": "https://byphdgllyk.gpcdn.net/hls/deeptotv/0_1/index.m3u8" }
+        ]
+      },
+
+      "Desh TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/DeshTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://bozztv.com/rongo/rongo-DeshTV/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Deshe Bideshe": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/DesheBidesheTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://dbcanada.sonarbanglatv.com/deshebideshe/dbtv/index.m3u8" }
+        ]
+      },
+
+      "Deshi TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/DeshiTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://deshitv.deshitv24.net/live/myStream/playlist.m3u8" }
+        ]
+      },
+
+      "Ekattor TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/EkattorTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1705/output/index.m3u8" }
+        ]
+      },
+
+      "G Series Drama": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/GSeriesDrama-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://vods2.aynaott.com/gseriesDrama/tracks-v1a1/mono.ts.m3u8" }
+        ]
+      },
+
+      "Galaxy TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/GalaxyTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/galaxytvbd.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "GTV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/GTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://tvsen1.aynaott.com/Ravc7gPCZpxk/tracks-v1a1/mono.ts.m3u8" },
+          { "name": "Server 2", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/gazibdz.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Independent TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/IndependentTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1704/output/index.m3u8" }
+        ]
+      },
+
+      "Islamic TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/IslamicTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1724/output/index.m3u8" }
+        ]
+      },
+
+      "Jamuna TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/JamunaTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1701/output/index.m3u8" }
+        ]
+      },
+
+      "Maasranga TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/MaasrangaTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1722/output/index.m3u8" }
+        ]
+      },
+
+      "Mohona TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/MohonaTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/mohonatv.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Movie Bangla": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/MovieBangla-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/moviebanglalink2.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "News 24": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/News24-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1708/output/index.m3u8" },
+          { "name": "Server 2", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/news24local.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Nexus TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/NexusTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/nexustv.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "NTV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/NTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1716/output/index.m3u8" }
+        ]
+      },
+
+      "Sangsad TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SangsadTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1713/output/index.m3u8" }
+        ]
+      },
+
+      "SATV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SATV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1720/output/index.m3u8" }
+        ]
+      },
+
+      "Somoy TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SomoyTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1702/output/index.m3u8" },
+          { "name": "Server 2", "url": "https://bozztv.com/rongo/rongo-somoy/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "SRK TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SRKTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://srknowapp.ncare.live/srktvhlswodrm/srktv.stream/playlist.m3u8" }
+        ]
+      },
+
+      "Star News": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/StarNews-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1710/output/index.m3u8" }
         ]
       }
       
