aa60005bbb18ef825c07461b739f4cd4460b8f20
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-02 15:43:11 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -10,18 +10,46 @@
           { "name": "Server 2", "url": "https://live-test.tsports.com/live-02.m3u8" }
         ]
       },
-      "Somoy TV": {
-        "logo": "https://example.com/somoytv-logo.png",
+      
+      "ATN News": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/ATNNews-logo.webp",
         "category": "Bangla",
         "servers": [
-          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1702/output/index.m3u8" }
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1706/output/index.m3u8" }
         ]
       },
-      "Independent TV": {
-        "logo": "https://example.com/independenttv-logo.png",
+
+      "Bangla Vision": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BanglaVision-logo.webp",
         "category": "Bangla",
         "servers": [
-          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1704/output/index.m3u8" }
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1715/output/index.m3u8" }
+        ]
+      },
+
+      "Boishakhi TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BoishakhiTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/boishakhitv-org.stream/playlist.m3u8" }
+        ]
+      },
+
+      "BTV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BTV-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1709/output/index.m3u8" }
+        ]
+      },
+
+      "Channel 9": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Channel9-logo.webp",
+        "category": "Bangla",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/channel9hd.stream/tracks-v1a1/mono.m3u8" },
+          { "name": "Server 2", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1729/output/index.m3u8" }
         ]
       }
+      
     }
