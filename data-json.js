cc4fac55ab8051daaa64320ea75abcbaf9026845
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-02 19:50:30 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -11,11 +11,59 @@
         ]
       },
 
+      "Bein Sports 1": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BeinSports1-logo.webp",
+        "category": "Sports",
+        "servers": [
+          { "name": "Server 1", "url": "https://maymunmatematigi.lol/static/bs1.m3u8" }
+        ]
+      },
+
+      "Bein Sports 2": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BeinSports2-logo.webp",
+        "category": "Sports",
+        "servers": [
+          { "name": "Server 1", "url": "https://maymunmatematigi.lol/static/bs2.m3u8" }
+        ]
+      },
+
+      "Bein Sports 3": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BeinSports3-logo.webp",
+        "category": "Sports",
+        "servers": [
+          { "name": "Server 1", "url": "https://maymunmatematigi.lol/static/bs3.m3u8" }
+        ]
+      },
+
+      "Bein Sports 4": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BeinSports4-logo.webp",
+        "category": "Sports",
+        "servers": [
+          { "name": "Server 1", "url": "https://maymunmatematigi.lol/static/bs4.m3u8" }
+        ]
+      },
+
+      "Bein Sports 5": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BeinSports5-logo.webp",
+        "category": "Sports",
+        "servers": [
+          { "name": "Server 1", "url": "https://maymunmatematigi.lol/static/bs5.m3u8" }
+        ]
+      },
+
       "Bangla Waz": {
         "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Waz-logo.webp",
-        "category": "Bangla",
+        "category": "Islamic",
         "servers": [
           { "name": "Server 1", "url": "https://live20.bozztv.com/giatvplayout7/giatv-209617/tracks-v1a1/mono.ts.m3u8" }
+        ]
+      },
+
+      "Peace TV Bangla": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/PeaceTVBangla-logo.webp",
+        "category": "Other",
+        "servers": [
+          { "name": "Server 1", "url": "https://dzkyvlfyge.erbvr.com/PeaceTvBangla/tracks-v3a1/mono.m3u8?sid=-AAs2UnhiEq4c2OtWsbmhQ" }
         ]
       },
       
@@ -81,7 +129,7 @@
         "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/ChannelS-logo.webp",
         "category": "Bangla",
         "servers": [
-          { "name": "Server 1", "url": "https://app.ncare.live/live-orgin/channels.stream/live-orgin/channels.stream/chunks.m3u8?nimblesessionid=1033225798" }
+          { "name": "Server 1", "url": "https://app.ncare.live/live-orgin/channels.stream/live-orgin/channels.stream/chunks.m3u8" }
         ]
       },
 
@@ -272,6 +320,230 @@
         "servers": [
           { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1710/output/index.m3u8" }
         ]
+      },
+
+"7S Music": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/7SMusic-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://mumt03.tangotv.in/7SMUSIC/tracks-v2a1/mono.m3u8" }
+        ]
+      },
+
+"Aakash Aath": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/AakashAath-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://mumt03.tangotv.in/AAKASHAATH/tracks-v2a1/mono.m3u8" }
+        ]
+      },
+
+"Amar Bangla": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/AmarBangla-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/amarbanglatv.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Balle Balle": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BalleBalle-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://cdn-4.pishow.tv/live/987/master.m3u8" }
+        ]
+      },
+
+      "Bangla Plus": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BanglaPlus-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://live-stream.utkalbongo.com/hls/livebanglatvstream.m3u8" }
+        ]
+      },
+
+      "Bhojpuri Cinema": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/BhojpuriCinema-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://live-bhojpuri.akamaized.net/liveabr/pub-iobhojpuqbu6yj/live_720p/chunks.m3u8" }
+        ]
+      },
+
+      "E24": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/E24-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://mumt04.tangotv.in/E24/tracks-v2a1/mono.m3u8" }
+        ]
+      },
+
+      "Enterr 10": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Enterr10-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://live-bangla.akamaized.net/liveabr/pub-iobanglakp3sff/live_720p/chunks.m3u8" }
+        ]
+      },
+
+      "Goldmines": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Goldmines-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://cdn-2.pishow.tv/live/1459/1459_1.m3u8" }
+        ]
+      },
+
+      "Goldmines Bollywood": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/GoldminesBollywood-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://mumt03.tangotv.in/GOLDMINESBOLLYWOOD/tracks-v2a1/mono.m3u8" }
+        ]
+      },
+
+      "Goldmines Movies": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/GoldminesMovies-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://cdn-2.pishow.tv/live/1461/1461_1.m3u8" }
+        ]
+      },
+
+      "Gopal Bhar": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/GopalBhar-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://live20.bozztv.com/giatvplayout7/giatv-209611/tracks-v1a1/mono.ts.m3u8" }
+        ]
+      },
+
+      "Hindi Movie": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/HindiMovie-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://live20.bozztv.com/giatvplayout7/giatv-209593/tracks-v1a1/mono.ts.m3u8" }
+        ]
+      },
+
+      "Jungle Book": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/JungleBook-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://cc-4bhi5osabejc9.akamaized.net/v1/master/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-4bhi5osabejc9/junglebook.m3u8" }
+        ]
+      },
+
+      "PTC Music": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/PTCMusic-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://d2lk5u59tns74c.cloudfront.net/out/v1/f913cf893c594f73b114216e74a2efbc/index_4.m3u8" }
+        ]
+      },
+
+      "Rongeen": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/Rongeen-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://cdn-4.pishow.tv/live/1029/master.m3u8" }
+        ]
+      },
+
+      "Samay Kolkata": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SamayKolkata-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://server.livelegitpro.in/samaykolkata/samaykolkata/index.m3u8" }
+        ]
+      },
+
+      "Sananda TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SanandaTV-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://live.sanandatelevision.in/sananda/tracks-v1a1/mono.ts.m3u8" }
+        ]
+      },
+
+      "Sangeet Bangla": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SangeetBangla-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://cdn-4.pishow.tv/live/1143/1143_1.m3u8" }
+        ]
+      },
+
+      "Sony Kal": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SonyKal-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://wurlsonypicturestv.global.transmit.live/hls/68deeb1c0238cda82df543dd/v1/spt_sonykal_1/lg_us/latest/main/hls/playlist.m3u8" }
+        ]
+      },
+
+      "Sony Max": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/SonyMax-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://padmaonline.duckdns.org:8088/SonyMaxHD/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Star Plus": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/StarPlus-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://padmaonline.duckdns.org:8088/StarPlusHD/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Tara TV": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/TaraTV-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://legitpro.co.in/taratv/taratv/index.m3u8" }
+        ]
+      },
+
+      "YRF Music": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/YRFMusic-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://amg01412-xiaomiasia-yrfmusic-xiaomi-1rmk2.amagi.tv/ts-eu-w1-n2/playlist/amg01412-xiaomiasia-yrfmusic-xiaomi/cb573e1e796c6d8884c83d67cef44382847b3dcb0e6c886470af4a9765d96300dbf9b9c8fd3ab13c1ec0468a710b6d5a4d1cd209af3c419b8b1dfd034f25536b9f9bb565da9f0211c9a06b5756af6a4208a55b05a7700079d6a05a2b8d2fba74005cb95a3e1862c3ed47ad662f9ad253d72a39e8fc8b307b24455a9c5a43e6a64cb7feb00c458d3ea8b8014c942b14fa83439fd501b3d485538312807ff847c8c7f08981188ae42e5293738aa19d71581fbba43bf9eb5cb8dd96683d99622034713f8482a08e85b81b253938d9ecc2be1f8f7b4b220cf30a14b3098c5b85378565ad4bd489de0e9cc9be603b5387cc527533a1e2af84fc7747d5e955939bbf222ce39c37704edd79d925243697b7ad7ee09747c6ef71c4dce17a49cabcd675aeb6d5cfc93e679e55f252c2b877286f700d03c979aa9b66da133f19c2c13e2e464448a19b1a91c8891e784e468895468f533ab7df998cd60a7b9b30401ac329997a157cc0315b79e706f08e7339da7144d6637db3731db46fd0f46cf9aed0e0461cb0ddcc4db62fccbd18ddfb07e63c6f094ce3bc7c11150c50dfec6f013539f044e2d2d8e8385c53bbb99ea8b23e52601819561e84ebe302f9df90537d1ca30b748015f3ed41abb006609cee250c7a8301ca48cc41e89652/82/1920x1080_5876488/index.m3u8" }
+        ]
+      },
+
+      "ZB Cartoon": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/ZBCartoon-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://server.zillarbarta.com/zbcatun/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Zee Bangla": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/ZeeBangla-logo.webp",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://padmaonline.duckdns.org:8088/ZeeBanglaHD/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Zoom": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/",
+        "category": "Indian",
+        "servers": [
+          { "name": "Server 1", "url": "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/zoomin.stream/tracks-v1a1/mono.m3u8" }
+        ]
+      },
+
+      "Al Jazeera": {
+        "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/AlJazeera-logo.webp",
+        "category": "Other",
+        "servers": [
+          { "name": "Server 1", "url": "https://owrcovcrpy.gpcdn.net/bpk-tv/1721/output/index.m3u8" }
+        ]
       }
-      
+
     }
