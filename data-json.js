7fe90ec3466a6ae548e104b4b9c850a57fe5528c
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-09 20:38:30 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -1,10 +1,10 @@
 {
       "Cricket": {
   "category": "Pin",
-  "status": "End",
+  "status": "",
   "time-date": "11:00 PM | 09-05-2026",
   "team": [
-    { "name": "Bangladesh", "logo": "#" },
+    { "name": "Bangladesh", "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLHk4-GCXixRBHaFQZjDv1AD_3qEB8sZPR5zGsdapg8A&s=10" },
     { "name": "Pakistan", "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrxsCzk80koxNm-PEl2CgxoJ3vR_hnTLpBzmf1XRKp5A&s=10" }
   ],
   "servers": [
