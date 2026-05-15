4c02d5e7579620a58ee9e449cae3946d63e5195a
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-15 14:58:36 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -1,5 +1,5 @@
 {
-      "Cricket": {
+      "Cricket | Indian Premier League": {
   "category": "Pin",
   "status": "",
   "time-date": "08:00 PM | 15-05-2026",
@@ -15,7 +15,7 @@
       "Cricket | ICC World Test Championship": {
   "category": "Pin",
   "status": "",
-  "time-date": "10:00 PM | 16-05-2026",
+  "time-date": "10:00 AM | 16-05-2026",
   "team": [
     { "name": "BAN", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776210/bangladesh.jpg" },
     { "name": "PAK", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776308/pakistan.jpg" }
