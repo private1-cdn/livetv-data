718d7568f92ed87aa3ba31e9957090ddaf5691fe
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-17 12:59:59 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -108,6 +108,19 @@
   ]
 },
 
+      "La Liga | Sports 3": {
+  "category": "Pin",
+  "status": "",
+  "time-date": "01:15 AM | 18-05-2026",
+  "team": [
+    { "name": "Barcelona", "logo": "https://img.sofascore.com/api/v1/team/2817/image" },
+    { "name": "Real Betis", "logo": "https://img.sofascore.com/api/v1/team/2816/image" }
+  ],
+  "servers": [
+    { "name": "Server 1", "url": "https://d3bb58xoj568xi.cloudfront.net/v1/manifest/3722c60a815c199d9c0ef36c5b73da68a62b09d1/cc-don7i7nmbho0q/fb7ba5c6-15a0-4c2a-84d4-5f82390085c6/2.m3u8" }
+  ]
+},
+
       
       "T Sports": {
         "logo": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/TSports-logo.webp",
