7ea8b4d9b03193cb317b1e2af2495c7a6e5c8174
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-05-20 17:01:34 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -2,7 +2,7 @@
 
   "Cricket | ICC World Test Championship": {
   "category": "Pin",
-  "status": "",
+  "status": "End",
   "time-date": "10:00 AM | 20-05-2026",
   "team": [
     { "name": "BAN", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776210/bangladesh.jpg" },
