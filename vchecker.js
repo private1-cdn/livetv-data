c81690ade07ceba4bed1e37a9d53b0f2dc1c19b0
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-04 15:32:56 +1000

    Update vchecker.js

--- vchecker.js
+++ vchecker.js
@@ -4,6 +4,7 @@ export const version = [
       "version": "1.2",
       "update": "04-06-2026",
       "icon": "https://img.magnific.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg",
+      "updateurl": "#",
       "telegram": "#",
       "web": "#"
     }
