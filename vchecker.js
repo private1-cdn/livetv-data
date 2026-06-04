eaa2516e8dab221a0799fbf8fb29797dac41ec80
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-04 13:30:21 +1000

    Update vchecker.js

--- vchecker.js
+++ vchecker.js
@@ -3,7 +3,7 @@ export const version = [
       "appname": "PinikBuzz",
       "version": "1.1",
       "update": "04-06-2026",
-      "icon": "https://img.magnific.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg"
+      "icon": "https://img.magnific.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg",
       "telegram": "#",
       "web": "#"
     }
