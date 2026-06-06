2fc49efd6e9eb703baa1c9e4cb60a1b6d052c6a3
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-06 18:02:56 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -9,21 +9,6 @@ export const welcome = [
 
 export const pin = [
   {
-    "Cricket | AFG Tour of IND": {
-      "category": "Pin",
-      "status": "",
-      "time-date": "10:00 AM | 06-06-2026",
-      "team": [
-        { "name": "IND", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776162/india.jpg" },
-        { "name": "AFG", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776177/afghanistan.jpg" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "https://saseries.akamaized.net/hls/live/2110097/T56A87p03/master.m3u8?hdnts=exp=1780743907~acl=/hls/*~hmac=6118cadab8dca5eb0b9f92750e2bedee87793a97ddae72f86447a46012dbd09b" },
-        { "name": "Server 2", "url": "https://tvsen5.aynaott.com/willowhd/index.m3u8" }
-      ]
-    }
-  },
-  {
     "Cricket | NZ Tour of ENG": {
       "category": "Pin",
       "status": "",
@@ -77,6 +62,21 @@ export const pin = [
       ],
       "servers": [
         { "name": "Server 1", "url": "#" }
+      ]
+    }
+  },
+  {
+    "Cricket | AFG Tour of IND": {
+      "category": "Pin",
+      "status": "End",
+      "time-date": "10:00 AM | 06-06-2026",
+      "team": [
+        { "name": "IND", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776162/india.jpg" },
+        { "name": "AFG", "logo": "https://static.cricbuzz.com/a/img/v1/0x0/i1/c776177/afghanistan.jpg" }
+      ],
+      "servers": [
+        { "name": "Server 1", "url": "https://saseries.akamaized.net/hls/live/2110097/T56A87p03/master.m3u8?hdnts=exp=1780743907~acl=/hls/*~hmac=6118cadab8dca5eb0b9f92750e2bedee87793a97ddae72f86447a46012dbd09b" },
+        { "name": "Server 2", "url": "https://tvsen5.aynaott.com/willowhd/index.m3u8" }
       ]
     }
   }
