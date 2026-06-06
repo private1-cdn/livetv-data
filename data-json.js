d65db4b00c917a06a71bde9a3eedf569d8812f5b
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-06 16:59:57 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -33,7 +33,7 @@ export const pin = [
         { "name": "NZ", "logo": "https://img.sofascore.com/api/v1/team/187755/image" }
       ],
       "servers": [
-        { "name": "Server 1", "url": "https://saseries.akamaized.net/hls/live/2110097/ICCt20-tapmad/master.m3u8?hdnts=exp=1780740907~acl=/hls/*~hmac=5bbfb10c9c5d21f090d8471348d820a1af720989c42ed158a8eaff9725cdd799" }
+        { "name": "Server 1", "url": "https://d8j84o343a5m2.cloudfront.net/live/testtapmad/master.m3u8?hdnts=exp=1780743907~acl=/hls/*~hmac=6118cadab8dca5eb0b9f92750e2bedee87793a97ddae72f86447a46012dbd09b" }
       ]
     }
   },
