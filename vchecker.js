427709d4409b2514b45e42c14b53527aa121bb62
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-08 00:51:33 +0600

    Update vchecker.js

--- vchecker.js
+++ vchecker.js
@@ -3,7 +3,7 @@ export const version = [
       "appname": "PinikBuzz",
       "version": "1.2",
       "update": "08-06-2026",
-      "icon": "https://img.magnific.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg",
+      "icon": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/512%C3%97512%20webp.webp",
       "updateurl": "https://github.com/private1-cdn/livetv-data/raw/refs/heads/main/apk/PinikBuzz_v1.2.apk",
       "telegram": "https://t.me/PinikBuzz",
       "web": "https://www.pinikbuzz.com"
