92a2153cd29834a24b95cbaf6b34d694d360a66e
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-07 11:37:21 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -23,6 +23,22 @@ export const pin = [
       ]
     }
   },
+
+  {
+    "Cricket | NZ Tour of ENG": {
+      "category": "Pin",
+      "status": "End",
+      "time-date": "04:00 PM | 06-06-2026",
+      "team": [
+        { "name": "ENG", "logo": "https://img.sofascore.com/api/v1/team/187757/image" },
+        { "name": "NZ", "logo": "https://img.sofascore.com/api/v1/team/187755/image" }
+      ],
+      "servers": [
+        { "name": "Server 1", "url": "https://tvsen5.aynaott.com/willowhd/index.m3u8" },
+        { "name": "Server 2", "url": "https://d8j84o343a5m2.cloudfront.net/live/testtapmad/master.m3u8?hdnts=exp=1780743907~acl=/hls/*~hmac=6118cadab8dca5eb0b9f92750e2bedee87793a97ddae72f86447a46012dbd09b" }
+      ]
+    }
+  },
   
   {
     "Football | 1": {
@@ -67,22 +83,8 @@ export const pin = [
         { "name": "Server 2", "url": "https://amitomar.bdixbd.net/hoichoi.pro1/tracks-v1a1/index.m3u8" }
       ]
     }
-  },
-  {
-    "Cricket | NZ Tour of ENG": {
-      "category": "Pin",
-      "status": "End",
-      "time-date": "04:00 PM | 06-06-2026",
-      "team": [
-        { "name": "ENG", "logo": "https://img.sofascore.com/api/v1/team/187757/image" },
-        { "name": "NZ", "logo": "https://img.sofascore.com/api/v1/team/187755/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "https://tvsen5.aynaott.com/willowhd/index.m3u8" },
-        { "name": "Server 2", "url": "https://d8j84o343a5m2.cloudfront.net/live/testtapmad/master.m3u8?hdnts=exp=1780743907~acl=/hls/*~hmac=6118cadab8dca5eb0b9f92750e2bedee87793a97ddae72f86447a46012dbd09b" }
-      ]
-    }
   }
+  
   
 ];
 
