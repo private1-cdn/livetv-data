89741ef41ef80f291fd2176e3e99483f69c9aebc
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-08 15:19:15 +0600

    Update vchecker.js

--- vchecker.js
+++ vchecker.js
@@ -1,10 +1,10 @@
 export const version = [
     {
       "appname": "PinikBuzz",
-      "version": "1.3",
+      "version": "1.4",
       "update": "08-06-2026",
       "icon": "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/channellogo/512%C3%97512%20webp.webp",
-      "updateurl": "https://github.com/private1-cdn/livetv-data/raw/refs/heads/main/apk/PinikBuzz_v1.3.apk",
+      "updateurl": "https://github.com/private1-cdn/livetv-data/raw/refs/heads/main/apk/PinikBuzz_v1.4.apk",
       "telegram": "https://t.me/PinikBuzz",
       "web": "https://www.pinikbuzz.com"
     }
