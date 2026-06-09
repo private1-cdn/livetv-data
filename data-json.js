5a9d3ac5a17fff951953f80664e3b66fcc2c6b99
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-09 11:35:08 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -25,17 +25,17 @@ export const pin = [
     }
   },
   {
-    "testing": {
+    "Football | International Friendly": {
       "category": "Pin",
-      "status": "Finished",
-      "start-in": "11:00 AM | 08-06-2026",
-      "end-in": "",
+      "status": "",
+      "start-in": "12:00 AM | 10-06-2026",
+      "end-in": "02:30 AM | 10-06-2026",
       "team": [
-        { "name": "BAN", "logo": "https://img.sofascore.com/api/v1/team/214194/image" },
-        { "name": "MDV", "logo": "https://img.sofascore.com/api/v1/team/214193/image" }
+        { "name": "SMR", "logo": "https://img.sofascore.com/api/v1/team/4833/image" },
+        { "name": "AZE", "logo": "https://img.sofascore.com/api/v1/team/4742/image" }
       ],
       "servers": [
-        { "name": "Server 1", "url": "https://pull.niues.live/live/stream-611528_lsd.m3u8?auth_key=1780850060-0-0-ed3b2b6c760037810bd0203a297dadf6" }
+        { "name": "Server 1", "url": "#" }
       ]
     }
   }
