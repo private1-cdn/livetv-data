1e62a6682f1973488f80c0ddffb67431bfb0947c
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-16 19:07:57 +0600

    Update data-json.js

--- data-json.js
+++ data-json.js
@@ -296,6 +296,15 @@ export const channels = [
     }
   },
   {
+    "ADN - A Sports": {
+      "logo": "ASports-logo.webp",
+      "category": "ADN - (Rakib)",
+      "servers": [
+        { "name": "Server 1", "url": "http://172.20.3.1:3255/A-SPORTS-HD/tracks-v1a1/mono.m3u8" }
+      ]
+    }
+  },
+  {
     "ADN - Zee Bangla Shonar": {
       "logo": "#",
       "category": "ADN - (Rakib)",
