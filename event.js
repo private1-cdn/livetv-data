36a7c4a0ccaeab7b0a089e6a3155c8eb30882bd8
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-17 10:39:27 +0600

    Update event.js

--- event.js
+++ event.js
@@ -26,224 +26,54 @@ export const liveevent = [
     }
   },
   {
-    "🏏 ICC Women's T20 World Cup - 2": {
-      "category": "Pin",
+    "2": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup 2",
       "status": "",
-      "start-in": "11:30 PM | 16-06-2026",
+      "start-in": "08:30 PM | 18-06-2026",
       "end-in": "",
       "team": [
-        { "name": "ENG-W", "logo": "https://img.sofascore.com/api/v1/team/213121/image" },
-        { "name": "IRE-W", "logo": "https://img.sofascore.com/api/v1/team/213116/image" }
+        {
+          "team-name": "NZ-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/213115/image"
+        },
+        {
+          "team-name": "SL-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/187806/image"
+        }
       ],
       "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  },
-  
-  
-  
-  
-
-
-
-
-  
-{
-    "⚽️ Fifa world cup 13": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "10:00 PM | 15-06-2026",
-      "end-in": "12:10 AM | 16-06-2026",
-      "team": [
-        { "name": "ESP", "logo": "https://img.sofascore.com/api/v1/team/4698/image" },
-        { "name": "CPV", "logo": "https://img.sofascore.com/api/v1/team/4753/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "http://84.17.50.102/fox/index.m3u8" },
-        { "name": "Server 2", "url": "https://d1g8wgjurz8via.cloudfront.net/bpk-tv/ColorsHD/default/master.m3u8" },
-        { "name": "Server 3", "url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8" },
-        { "name": "Server 4", "url": "http://198.195.239.50:8095/tsports/tracks-v1a1/mono.m3u8" },
-        { "name": "Server 5", "url": "https://m.mxonlive.xyz/nexusstream/ch08.m3u8" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 14": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "01:00 AM | 16-06-2026",
-      "end-in": "03:10 AM | 16-06-2026",
-      "team": [
-        { "name": "BEL", "logo": "https://img.sofascore.com/api/v1/team/4717/image" },
-        { "name": "EGY", "logo": "https://img.sofascore.com/api/v1/team/4758/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "http://84.17.50.102/fox/index.m3u8" },
-        { "name": "Server 2", "url": "https://d1g8wgjurz8via.cloudfront.net/bpk-tv/ColorsHD/default/master.m3u8" },
-        { "name": "Server 3", "url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8" },
-        { "name": "Server 4", "url": "http://198.195.239.50:8095/tsports/tracks-v1a1/mono.m3u8" },
-        { "name": "Server 5", "url": "https://m.mxonlive.xyz/nexusstream/ch08.m3u8" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 15": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "04:00 AM | 16-06-2026",
-      "end-in": "06:10 AM | 16-06-2026",
-      "team": [
-        { "name": "KSA", "logo": "https://img.sofascore.com/api/v1/team/4834/image" },
-        { "name": "URU", "logo": "https://img.sofascore.com/api/v1/team/4725/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "http://84.17.50.102/fox/index.m3u8" },
-        { "name": "Server 2", "url": "https://d1g8wgjurz8via.cloudfront.net/bpk-tv/ColorsHD/default/master.m3u8" },
-        { "name": "Server 3", "url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8" },
-        { "name": "Server 4", "url": "http://198.195.239.50:8095/tsports/tracks-v1a1/mono.m3u8" },
-        { "name": "Server 5", "url": "https://m.mxonlive.xyz/nexusstream/ch08.m3u8" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 16": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "07:00 AM | 16-06-2026",
-      "end-in": "09:10 AM | 16-06-2026",
-      "team": [
-        { "name": "IRN", "logo": "https://img.sofascore.com/api/v1/team/4766/image" },
-        { "name": "NZL", "logo": "https://img.sofascore.com/api/v1/team/4784/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "http://84.17.50.102/fox/index.m3u8" },
-        { "name": "Server 2", "url": "https://d1g8wgjurz8via.cloudfront.net/bpk-tv/ColorsHD/default/master.m3u8" },
-        { "name": "Server 3", "url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8" },
-        { "name": "Server 4", "url": "http://198.195.239.50:8095/tsports/tracks-v1a1/mono.m3u8" },
-        { "name": "Server 5", "url": "https://m.mxonlive.xyz/nexusstream/ch08.m3u8" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 17": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "01:00 AM | 17-06-2026",
-      "end-in": "03:10 AM | 17-06-2026",
-      "team": [
-        { "name": "FRA", "logo": "https://img.sofascore.com/api/v1/team/4481/image" },
-        { "name": "SEN", "logo": "https://img.sofascore.com/api/v1/team/4739/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
+        {
+          "name": "Server 1",
+          "url": "#"
+        }
       ]
     }
   },
   {
-    "⚽️ Fifa world cup 18": {
-      "category": "Pin",
+    "3": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup 3",
       "status": "",
-      "start-in": "04:00 AM | 17-06-2026",
-      "end-in": "06:10 AM | 17-06-2026",
+      "start-in": "09:30 PM | 18-06-2026",
+      "end-in": "",
       "team": [
-        { "name": "IRQ", "logo": "https://img.sofascore.com/api/v1/team/4767/image" },
-        { "name": "NOR", "logo": "https://img.sofascore.com/api/v1/team/4475/image" }
+        {
+          "team-name": "NZ-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/213115/image"
+        },
+        {
+          "team-name": "SL-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/187806/image"
+        }
       ],
       "servers": [
-        { "name": "Server 1", "url": "#" }
+        {
+          "name": "Server 1",
+          "url": "#"
+        }
       ]
     }
   },
-{
-    "⚽️ Fifa world cup 19": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "07:00 AM | 17-06-2026",
-      "end-in": "09:10 AM | 17-06-2026",
-      "team": [
-        { "name": "ARG", "logo": "https://img.sofascore.com/api/v1/team/4819/image" },
-        { "name": "ALG", "logo": "https://img.sofascore.com/api/v1/team/4691/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 20": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "10:00 AM | 17-06-2026",
-      "end-in": "12:10 PM | 17-06-2026",
-      "team": [
-        { "name": "AUT", "logo": "https://img.sofascore.com/api/v1/team/4718/image" },
-        { "name": "JOR", "logo": "https://img.sofascore.com/api/v1/team/4771/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 21": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "11:00 PM | 17-06-2026",
-      "end-in": "01:10 AM | 18-06-2026",
-      "team": [
-        { "name": "POR", "logo": "https://img.sofascore.com/api/v1/team/4704/image" },
-        { "name": "COD", "logo": "https://img.sofascore.com/api/v1/team/4823/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 22": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "02:00 AM | 18-06-2026",
-      "end-in": "04:10 AM | 18-06-2026",
-      "team": [
-        { "name": "ENG", "logo": "https://img.sofascore.com/api/v1/team/4713/image" },
-        { "name": "CRO", "logo": "https://img.sofascore.com/api/v1/team/4715/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 23": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "05:00 AM | 18-06-2026",
-      "end-in": "07:10 AM | 18-06-2026",
-      "team": [
-        { "name": "GHA", "logo": "https://img.sofascore.com/api/v1/team/4764/image" },
-        { "name": "PAN", "logo": "https://img.sofascore.com/api/v1/team/5164/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  },
-{
-    "⚽️ Fifa world cup 24": {
-      "category": "Pin",
-      "status": "",
-      "start-in": "08:00 AM | 18-06-2026",
-      "end-in": "10:10 AM | 18-06-2026",
-      "team": [
-        { "name": "UZB", "logo": "https://img.sofascore.com/api/v1/team/4723/image" },
-        { "name": "COL", "logo": "https://img.sofascore.com/api/v1/team/4820/image" }
-      ],
-      "servers": [
-        { "name": "Server 1", "url": "#" }
-      ]
-    }
-  }
   
 ];
