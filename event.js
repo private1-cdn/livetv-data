70f189388c9e2833601a3001a1bac2b70cb65307
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-17 11:09:37 +0600

    Update event.js

--- event.js
+++ event.js
@@ -74,6 +74,81 @@ export const liveevent = [
         }
       ]
     }
+  },
+  {
+    "4": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup",
+      "status": "",
+      "start-in": "07:30 PM | 18-06-2026",
+      "end-in": "",
+      "team": [
+        {
+          "team-name": "NZ-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/213115/image"
+        },
+        {
+          "team-name": "SL-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/187806/image"
+        }
+      ],
+      "servers": [
+        {
+          "name": "Server 1",
+          "url": "#"
+        }
+      ]
+    }
+  },
+  {
+    "5": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup 2",
+      "status": "",
+      "start-in": "08:30 PM | 18-06-2026",
+      "end-in": "",
+      "team": [
+        {
+          "team-name": "NZ-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/213115/image"
+        },
+        {
+          "team-name": "SL-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/187806/image"
+        }
+      ],
+      "servers": [
+        {
+          "name": "Server 1",
+          "url": "#"
+        }
+      ]
+    }
+  },
+  {
+    "6": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup 3",
+      "status": "",
+      "start-in": "09:30 PM | 18-06-2026",
+      "end-in": "",
+      "team": [
+        {
+          "team-name": "NZ-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/213115/image"
+        },
+        {
+          "team-name": "SL-W",
+          "team-logo": "https://img.sofascore.com/api/v1/team/187806/image"
+        }
+      ],
+      "servers": [
+        {
+          "name": "Server 1",
+          "url": "#"
+        }
+      ]
+    }
   }
   
 ];
