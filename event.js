079a0a95ff2e006adab66bae297d2af3c57de448
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-25 07:34:21 +0600

    Update event.js

--- event.js
+++ event.js
@@ -65,6 +65,57 @@ export const liveevent = [
         
       ]
     }
+  },
+
+  {
+    "7": {
+      "category": "Cricket",
+      "title": "3rd T20",
+      "status": "",
+      "start-in": "08:00 PM | 25-06-2026",
+      "end-in": "",
+      "team": [
+        { "team-name": "West Indies", "team-logo": "https://img.sofascore.com/api/v1/team/187760/image" },
+        { "team-name": "Sri Lanka", "team-logo": "https://img.sofascore.com/api/v1/team/187756/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "8": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup",
+      "status": "",
+      "start-in": "07:30 PM | 25-06-2026",
+      "end-in": "",
+      "team": [
+        { "team-name": "BAN-W", "team-logo": "https://img.sofascore.com/api/v1/team/187809/image" },
+        { "team-name": "IND-W", "team-logo": "https://img.sofascore.com/api/v1/team/213119/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "9": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup",
+      "status": "",
+      "start-in": "11:30 PM | 25-06-2026",
+      "end-in": "",
+      "team": [
+        { "team-name": "SA-W", "team-logo": "https://img.sofascore.com/api/v1/team/213118/image" },
+        { "team-name": "NED-W", "team-logo": "https://img.sofascore.com/api/v1/team/417753/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
   }
   
 ];
