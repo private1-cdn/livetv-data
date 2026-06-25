cf12df9d854c0670b824ca6d45652a5b372240d0
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-25 19:27:22 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,73 +1,6 @@
 export const liveevent = [
 
   {
-    "5": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "07:00 AM | 25-06-2026",
-      "end-in": "09:10 AM | 25-06-2026",
-      "team": [
-        { "team-name": "Czechia", "team-logo": "https://img.sofascore.com/api/v1/team/4714/image" },
-        { "team-name": "Mexico", "team-logo": "https://img.sofascore.com/api/v1/team/4781/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8"
-        },
-        {
-          "server-name": "BTV",
-          "stream-url": "https://proxy.outflanktv.workers.dev/?url=https://tv.beyondtaxconsultants.com/api/robi/lps/http://198.195.239.50:8095/btv/tracks-v1a1/mono.m3u8"
-        },
-        {
-          "server-name": "FOX",
-          "stream-url": "http://84.17.50.102/fox/index.m3u8"
-        },
-        {
-          "server-name": "i Screen",
-          "stream-url": "https://sm-monirul.top/ott/iscreen/index.m3u8"
-        },
-        {
-          "server-name": "T Sports",
-          "stream-url": "https://trs1.aynaott.com/tsports/tracks-v1a1/mono.ts.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "6": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "07:00 AM | 25-06-2026",
-      "end-in": "09:10 AM | 25-06-2026",
-      "team": [
-        { "team-name": "South Africa", "team-logo": "https://img.sofascore.com/api/v1/team/4736/image" },
-        { "team-name": "South Korea", "team-logo": "https://img.sofascore.com/api/v1/team/4735/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Soco",
-          "stream-url": "https://pull.niues.live/live/stream-9912091_lsd.m3u8?auth_key=1782343582-0-0-f580dab032797e980aad9f346e451634"
-        },
-
-        {
-          "server-name": "S8 TV",
-          "stream-url": "https://hls.lauthaitv.cc/live/rsa-kor-wc-nts/index.m3u8"
-        },
-        {
-          "server-name": "CCTV 5",
-          "stream-url": "https://play1.gzxdby.com/live/783234345958_4547667094.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
     "7": {
       "category": "Cricket",
       "title": "3rd T20",
@@ -96,7 +29,10 @@ export const liveevent = [
         { "team-name": "IND-W", "team-logo": "https://img.sofascore.com/api/v1/team/213119/image" }
       ],
       "servers": [
-        
+        {
+          "server-name": "Willow",
+          "stream-url": "https://trs1.aynaott.com/willowhd/tracks-v1a1/mono.ts.m3u8"
+        }
       ]
     }
   },
