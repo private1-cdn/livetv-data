26580aa7d3b923b298f83442252dc93fd3fc4f52
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-27 01:23:13 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,43 +1,6 @@
 export const liveevent = [
 
   {
-    "16": {
-      "category": "Cricket",
-      "title": "T20 International Series",
-      "status": "",
-      "start-in": "06:30 PM | 26-06-2026",
-      "end-in": "",
-      "team": [
-        { "team-name": "India", "team-logo": "https://img.sofascore.com/api/v1/team/187765/image" },
-        { "team-name": "Ireland", "team-logo": "https://img.sofascore.com/api/v1/team/187766/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "TNT Sports 1",
-          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/iad-nitro/live/clients/dash/enc/vaplpo3app/out/v1/49b4d538ec854efa90d45084866cf7f9/cenc.mpd",
-          "keyid": "d14658a0ede94debcb4013b2056ba6d1",
-          "key": "60838a3176127fed0eaf038a5575de11"
-        },
-        {
-          "server-name": "Willow",
-          "mpd-stream-url": "https://abkyrm4aaaaaaaamfyfksth3wr44v.ta.bia-cf.live.pv-cdn.net/iad-nitro/live/clients/dash/enc/wkyr0oiayl/out/v1/ab6df1604fe34712a1a5bc16cc8489c3/cenc.mpd",
-          "keyid": "fff0c88530698729506de226786a0ce2",
-          "key": "473273ba6a5323ff4174a55ab2e3dfca"
-        },
-        {
-          "server-name": "Tapmad",
-          "stream-url": "https://saseries.akamaized.net/hls/live/2110097/T56A87p03/hdntl=exp=1782564167~acl=%2f*~data=hdntl~hmac=3109c6347bf8c93f1c124ba2c0851d1d090ed35cf8a5f22d15bc72db8592fc46/level_0.m3u8"
-        },
-        {
-          "server-name": "Willow",
-          "stream-url": "https://trs1.aynaott.com/willowhd/tracks-v1a1/mono.ts.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
     "17": {
       "category": "Cricket",
       "title": "ICC Women's T20 World Cup",
