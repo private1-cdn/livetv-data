572dc79309bc28d4f6ddc814ef7f6683177cd2a0
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-26 15:54:05 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,60 +1,6 @@
 export const liveevent = [
 
   {
-    "14": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "08:00 AM | 26-06-2026",
-      "end-in": "10:10 AM | 26-06-2026",
-      "team": [
-        { "team-name": "Paraguay", "team-logo": "https://img.sofascore.com/api/v1/team/4789/image" },
-        { "team-name": "Australia", "team-logo": "https://img.sofascore.com/api/v1/team/4741/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "CCTV 5",
-          "stream-url": "https://play1.gzxdby.com/live/783234345958_4547667094.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "15": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "08:00 AM | 26-06-2026",
-      "end-in": "10:10 AM | 26-06-2026",
-      "team": [
-        { "team-name": "Turkey", "team-logo": "https://img.sofascore.com/api/v1/team/4700/image" },
-        { "team-name": "USA", "team-logo": "https://img.sofascore.com/api/v1/team/4724/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8"
-        },
-        {
-          "server-name": "FOX",
-          "stream-url": "http://84.17.50.102/fox/index.m3u8"
-        },
-        {
-          "server-name": "T Sports",
-          "stream-url": "https://trs1.aynaott.com/tsports/tracks-v1a1/mono.ts.m3u8"
-        },
-        {
-          "server-name": "Tofee",
-          "stream-url": "https://sm-monirul.top/tof/live/toffee5/index.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
     "16": {
       "category": "Cricket",
       "title": "T20 International Series",
@@ -81,6 +27,108 @@ export const liveevent = [
       "team": [
         { "team-name": "SL-W", "team-logo": "https://img.sofascore.com/api/v1/team/187806/image" },
         { "team-name": "SCO-W", "team-logo": "https://img.sofascore.com/api/v1/team/453162/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "18": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "01:00 AM | 27-06-2026",
+      "end-in": "03:10 AM | 27-06-2026",
+      "team": [
+        { "team-name": "Norway", "team-logo": "https://img.sofascore.com/api/v1/team/4475/image" },
+        { "team-name": "France", "team-logo": "https://img.sofascore.com/api/v1/team/4481/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "19": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "01:00 AM | 27-06-2026",
+      "end-in": "03:10 AM | 27-06-2026",
+      "team": [
+        { "team-name": "Senegal", "team-logo": "https://img.sofascore.com/api/v1/team/4739/image" },
+        { "team-name": "Iraq", "team-logo": "https://img.sofascore.com/api/v1/team/4767/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "20": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "06:00 AM | 27-06-2026",
+      "end-in": "08:10 AM | 27-06-2026",
+      "team": [
+        { "team-name": "Uruguay", "team-logo": "https://img.sofascore.com/api/v1/team/4725/image" },
+        { "team-name": "Spain", "team-logo": "https://img.sofascore.com/api/v1/team/4698/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "21": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "06:00 AM | 27-06-2026",
+      "end-in": "08:10 AM | 27-06-2026",
+      "team": [
+        { "team-name": "Cape Verde", "team-logo": "https://img.sofascore.com/api/v1/team/4753/image" },
+        { "team-name": "Saudi Arabia", "team-logo": "https://img.sofascore.com/api/v1/team/4834/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "22": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "09:00 AM | 27-06-2026",
+      "end-in": "11:10 AM | 27-06-2026",
+      "team": [
+        { "team-name": "Egypt", "team-logo": "https://img.sofascore.com/api/v1/team/4758/image" },
+        { "team-name": "Iran", "team-logo": "https://img.sofascore.com/api/v1/team/4766/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "23": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "09:00 AM | 27-06-2026",
+      "end-in": "11:10 AM | 27-06-2026",
+      "team": [
+        { "team-name": "New Zealand", "team-logo": "https://img.sofascore.com/api/v1/team/4784/image" },
+        { "team-name": "Belgium", "team-logo": "https://img.sofascore.com/api/v1/team/4717/image" }
       ],
       "servers": [
         
