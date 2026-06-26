a0c84e6d3377d7cb9e86cfabd9c6e24202269074
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-26 08:10:50 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,141 +1,6 @@
 export const liveevent = [
 
   {
-    "7": {
-      "category": "Cricket",
-      "title": "3rd T20",
-      "status": "",
-      "start-in": "08:00 PM | 25-06-2026",
-      "end-in": "",
-      "team": [
-        { "team-name": "West Indies", "team-logo": "https://img.sofascore.com/api/v1/team/187760/image" },
-        { "team-name": "Sri Lanka", "team-logo": "https://img.sofascore.com/api/v1/team/187756/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Willow",
-          "stream-url": "https://trs1.aynaott.com/willowhd/tracks-v1a1/mono.ts.m3u8"
-        },
-        {
-          "server-name": "Tapmad",
-          "stream-url": "https://saseries.akamaized.net/hls/live/2110097/ICCt20-tapmad/hdntl=exp=1782498756~acl=%2f*~data=hdntl~hmac=4261ebf5277aa55f3610401bf438e869d037ce64e3aeec2473b4f502775f38cc/level_0.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  
-
-  {
-    "10": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "02:00 AM | 26-06-2026",
-      "end-in": "04:10 AM | 26-06-2026",
-      "team": [
-        { "team-name": "Curacao", "team-logo": "https://img.sofascore.com/api/v1/team/55827/image" },
-        { "team-name": "Cote D'Ivoire", "team-logo": "https://img.sofascore.com/api/v1/team/4768/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "S8 TV",
-          "stream-url": "https://hls.lauthaitv.cc/live/cuv-civ-wc-nts/index.m3u8"
-        },
-        {
-          "server-name": "OK WIN TV",
-          "stream-url": "https://ivs-play.knozi.top/live/live_1782348592667_acbvhjkv.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8"
-        },
-        {
-          "server-name": "Tofee",
-          "stream-url": "https://sm-monirul.top/tof/live/ecuvscur/index.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "11": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "02:00 AM | 26-06-2026",
-      "end-in": "04:10 AM | 26-06-2026",
-      "team": [
-        { "team-name": "Ecuador", "team-logo": "https://img.sofascore.com/api/v1/team/4757/image" },
-        { "team-name": "Germany", "team-logo": "https://img.sofascore.com/api/v1/team/4711/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Special Server",
-          "stream-url": "https://wccdn.djdoolky76.net/stream/wctv2/index.m3u8"
-        },
-        {
-          "server-name": "OK WIN TV",
-          "stream-url": "https://ivs-play.knozi.top/live/live_1782348606094_2dgv1u5n.m3u8"
-        },
-        {
-          "server-name": "S8 TV",
-          "stream-url": "https://hls.lauthaitv.cc/live/ecu-ger-wc-cat/index.m3u8"
-        },
-        {
-          "server-name": "FOX",
-          "stream-url": "http://84.17.50.102/fox/index.m3u8"
-        },
-        {
-          "server-name": "T Sports",
-          "stream-url": "https://trs1.aynaott.com/tsports/tracks-v1a1/mono.ts.m3u8"
-        },
-        {
-          "server-name": "CCTV 5",
-          "stream-url": "https://play1.gzxdby.com/live/783234345958_4547667094.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "12": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "05:00 AM | 26-06-2026",
-      "end-in": "07:10 AM | 26-06-2026",
-      "team": [
-        { "team-name": "Japan", "team-logo": "https://img.sofascore.com/api/v1/team/4770/image" },
-        { "team-name": "Sweden", "team-logo": "https://img.sofascore.com/api/v1/team/4688/image" }
-      ],
-      "servers": [
-        
-      ]
-    }
-  },
-
-  {
-    "13": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "05:00 AM | 26-06-2026",
-      "end-in": "07:10 AM | 26-06-2026",
-      "team": [
-        { "team-name": "Tunisia", "team-logo": "https://img.sofascore.com/api/v1/team/4729/image" },
-        { "team-name": "Netherlands", "team-logo": "https://img.sofascore.com/api/v1/team/4705/image" }
-      ],
-      "servers": [
-        
-      ]
-    }
-  },
-
-  {
     "14": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
@@ -147,6 +12,10 @@ export const liveevent = [
         { "team-name": "Australia", "team-logo": "https://img.sofascore.com/api/v1/team/4741/image" }
       ],
       "servers": [
+        {
+          "server-name": "CCTV 5",
+          "stream-url": "https://play1.gzxdby.com/live/783234345958_4547667094.m3u8"
+        }
         
       ]
     }
@@ -164,6 +33,22 @@ export const liveevent = [
         { "team-name": "USA", "team-logo": "https://img.sofascore.com/api/v1/team/4724/image" }
       ],
       "servers": [
+        {
+          "server-name": "Bein Sports 1",
+          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8"
+        },
+        {
+          "server-name": "FOX",
+          "stream-url": "http://84.17.50.102/fox/index.m3u8"
+        },
+        {
+          "server-name": "T Sports",
+          "stream-url": "https://trs1.aynaott.com/tsports/tracks-v1a1/mono.ts.m3u8"
+        },
+        {
+          "server-name": "Tofee",
+          "stream-url": "https://sm-monirul.top/tof/live/toffee5/index.m3u8"
+        }
         
       ]
     }
