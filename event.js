4b8f6f2046ec3b6c0df71ed7973c12e5168224f1
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-26 18:36:15 +0600

    Update event.js

--- event.js
+++ event.js
@@ -18,7 +18,16 @@ export const liveevent = [
           "keyid": "d14658a0ede94debcb4013b2056ba6d1",
           "key": "60838a3176127fed0eaf038a5575de11"
         },
-        
+        {
+          "server-name": "Willow",
+          "mpd-stream-url": "https://abkyrm4aaaaaaaamfyfksth3wr44v.ta.bia-cf.live.pv-cdn.net/iad-nitro/live/clients/dash/enc/wkyr0oiayl/out/v1/ab6df1604fe34712a1a5bc16cc8489c3/cenc.mpd",
+          "keyid": "fff0c88530698729506de226786a0ce2",
+          "key": "473273ba6a5323ff4174a55ab2e3dfca"
+        },
+        {
+          "server-name": "Tapmad",
+          "stream-url": "https://saseries.akamaized.net/hls/live/2110097/T56A87p03/master.m3u8?hdnts=exp=1782477307~acl=/hls/*~hmac=0d0f57c57ed54760cc45500403094d790161ead96925c2cc45b7ff84744c0f2e"
+        },
         {
           "server-name": "Willow",
           "stream-url": "https://trs1.aynaott.com/willowhd/tracks-v1a1/mono.ts.m3u8"
