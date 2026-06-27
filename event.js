dbf96d57fcf4f394639000504b347a6f3b2040aa
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-27 22:54:02 +0600

    Update event.js

--- event.js
+++ event.js
@@ -19,6 +19,108 @@ export const liveevent = [
         
       ]
     }
+  },
+
+  {
+    "25": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "03:00 AM | 28-06-2026",
+      "end-in": "05:10 AM | 28-06-2026",
+      "team": [
+        { "team-name": "Croatia", "team-logo": "https://img.sofascore.com/api/v1/team/4715/image" },
+        { "team-name": "Ghana", "team-logo": "https://img.sofascore.com/api/v1/team/4764/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "26": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "03:00 AM | 28-06-2026",
+      "end-in": "05:10 AM | 28-06-2026",
+      "team": [
+        { "team-name": "Panama", "team-logo": "https://img.sofascore.com/api/v1/team/5164/image" },
+        { "team-name": "England", "team-logo": "https://img.sofascore.com/api/v1/team/4713/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "27": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "05:30 AM | 28-06-2026",
+      "end-in": "07:40 AM | 28-06-2026",
+      "team": [
+        { "team-name": "Colombia", "team-logo": "https://img.sofascore.com/api/v1/team/4820/image" },
+        { "team-name": "Portugal", "team-logo": "https://img.sofascore.com/api/v1/team/4704/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "28": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "05:30 AM | 28-06-2026",
+      "end-in": "07:40 AM | 28-06-2026",
+      "team": [
+        { "team-name": "DR Congo", "team-logo": "https://img.sofascore.com/api/v1/team/4823/image" },
+        { "team-name": "Uzbekistan", "team-logo": "https://img.sofascore.com/api/v1/team/4723/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "29": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "08:00 AM | 28-06-2026",
+      "end-in": "10:10 AM | 28-06-2026",
+      "team": [
+        { "team-name": "Algeria", "team-logo": "https://img.sofascore.com/api/v1/team/4691/image" },
+        { "team-name": "Austria", "team-logo": "https://img.sofascore.com/api/v1/team/4718/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
+  },
+
+  {
+    "30": {
+      "category": "Football",
+      "title": "Fifa World Cup 2026",
+      "status": "",
+      "start-in": "08:00 AM | 28-06-2026",
+      "end-in": "10:10 AM | 28-06-2026",
+      "team": [
+        { "team-name": "Argentina", "team-logo": "https://img.sofascore.com/api/v1/team/4819/image" },
+        { "team-name": "Jordan", "team-logo": "https://img.sofascore.com/api/v1/team/4771/image" }
+      ],
+      "servers": [
+        
+      ]
+    }
   }
   
 ];
