ff462ae6d36a9d82920cbbb3a0090fa318432c53
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-27 12:13:48 +0600

    Update event.js

--- event.js
+++ event.js
@@ -2,28 +2,17 @@ export const liveevent = [
 
   {
     "22": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup",
       "status": "",
-      "start-in": "09:00 AM | 27-06-2026",
-      "end-in": "11:10 AM | 27-06-2026",
+      "start-in": "03:30 PM | 27-06-2026",
+      "end-in": "07:30 PM | 27-06-2026",
       "team": [
-        { "team-name": "Egypt", "team-logo": "https://img.sofascore.com/api/v1/team/4758/image" },
-        { "team-name": "Iran", "team-logo": "https://img.sofascore.com/api/v1/team/4766/image" }
+        { "team-name": "PAK-W", "team-logo": "https://img.sofascore.com/api/v1/team/187808/image" },
+        { "team-name": "NED-W", "team-logo": "https://img.sofascore.com/api/v1/team/417753/image" }
       ],
       "servers": [
-        {
-          "server-name": "Sport TV 1",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=https://1nyaler.streamhostingcdn.top/stream/33/index.m3u8"
-        },
-        {
-          "server-name": "Unite8 Sports 2",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=http://212.102.34.8:9080/AndFlixHD/video.m3u8"
-        },
-        {
-          "server-name": "BTV",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=http://114.130.57.233:8080/BTV/video.m3u8?token=SkQuhAXZxgBan1"
-        }
+        
         
       ]
     }
@@ -31,28 +20,35 @@ export const liveevent = [
 
   {
     "23": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup",
       "status": "",
-      "start-in": "09:00 AM | 27-06-2026",
-      "end-in": "11:10 AM | 27-06-2026",
+      "start-in": "07:30 PM | 27-06-2026",
+      "end-in": "11:30 PM | 27-06-2026",
       "team": [
-        { "team-name": "New Zealand", "team-logo": "https://img.sofascore.com/api/v1/team/4784/image" },
-        { "team-name": "Belgium", "team-logo": "https://img.sofascore.com/api/v1/team/4717/image" }
+        { "team-name": "WI-W", "team-logo": "https://img.sofascore.com/api/v1/team/213120/image" },
+        { "team-name": "IRE-W", "team-logo": "https://img.sofascore.com/api/v1/team/213116/image" }
       ],
       "servers": [
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=https%3A%2F%2F1nyaler.streamhostingcdn.top%2Fstream%2F23%2Findex.m3u8"
-        },
-        {
-          "server-name": "FOX",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=http://84.17.50.102/fox/index.m3u8"
-        },
-        {
-          "server-name": "TVP Sport",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=https://1nyaler.streamhostingcdn.top/stream/89/index.m3u8"
-        }
+        
+        
+      ]
+    }
+  },
+
+  {
+    "24": {
+      "category": "Cricket",
+      "title": "ICC Women's T20 World Cup",
+      "status": "",
+      "start-in": "11:30 PM | 27-06-2026",
+      "end-in": "03:30 AM | 28-06-2026",
+      "team": [
+        { "team-name": "ENG-W", "team-logo": "https://img.sofascore.com/api/v1/team/213121/image" },
+        { "team-name": "NZ-W", "team-logo": "https://img.sofascore.com/api/v1/team/213115/image" }
+      ],
+      "servers": [
+        
         
       ]
     }
