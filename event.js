820be47abd64e33b6202c51534d84ee563be6e4f
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-27 22:40:02 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,33 +1,6 @@
 export const liveevent = [
 
   {
-    "23": {
-      "category": "Cricket",
-      "title": "ICC Women's T20 World Cup",
-      "status": "",
-      "start-in": "07:30 PM | 27-06-2026",
-      "end-in": "11:30 PM | 27-06-2026",
-      "team": [
-        { "team-name": "WI-W", "team-logo": "https://img.sofascore.com/api/v1/team/213120/image" },
-        { "team-name": "IRE-W", "team-logo": "https://img.sofascore.com/api/v1/team/213116/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Willow",
-          "mpd-stream-url": "https://abkyrm4aaaaaaaamfyfksth3wr44v.ta.bia-cf.live.pv-cdn.net/iad-nitro/live/clients/dash/enc/fpfokpwebt/out/v1/3c0e5eba7c4944298dc46b564a3226e1/cenc.mpd",
-          "keyid": "15cb6a46b28fc380faab795e4ade11c7",
-          "key": "bc0a81fd5fd0c056f0e25bea5a9deef1"
-        },
-        {
-          "server-name": "Willow",
-          "stream-url": "https://warm-caverns-48629-92fab798385f.herokuapp.com/https://d36r8jifhgsk5j.cloudfront.net/Willow_TV1080p.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
     "24": {
       "category": "Cricket",
       "title": "ICC Women's T20 World Cup",
@@ -39,7 +12,10 @@ export const liveevent = [
         { "team-name": "NZ-W", "team-logo": "https://img.sofascore.com/api/v1/team/213115/image" }
       ],
       "servers": [
-        
+        {
+          "server-name": "Willow",
+          "stream-url": "https://warm-caverns-48629-92fab798385f.herokuapp.com/https://d36r8jifhgsk5j.cloudfront.net/Willow_TV1080p.m3u8"
+        }
         
       ]
     }
