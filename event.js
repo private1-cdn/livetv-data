e365ac688f8a9096e066997384e88231498c3522
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-27 18:45:58 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,15 +1,15 @@
 export const liveevent = [
 
   {
-    "22": {
+    "23": {
       "category": "Cricket",
       "title": "ICC Women's T20 World Cup",
       "status": "",
-      "start-in": "03:30 PM | 27-06-2026",
-      "end-in": "07:30 PM | 27-06-2026",
+      "start-in": "07:30 PM | 27-06-2026",
+      "end-in": "11:30 PM | 27-06-2026",
       "team": [
-        { "team-name": "PAK-W", "team-logo": "https://img.sofascore.com/api/v1/team/187808/image" },
-        { "team-name": "NED-W", "team-logo": "https://img.sofascore.com/api/v1/team/417753/image" }
+        { "team-name": "WI-W", "team-logo": "https://img.sofascore.com/api/v1/team/213120/image" },
+        { "team-name": "IRE-W", "team-logo": "https://img.sofascore.com/api/v1/team/213116/image" }
       ],
       "servers": [
         {
@@ -22,24 +22,6 @@ export const liveevent = [
           "server-name": "Willow",
           "stream-url": "https://warm-caverns-48629-92fab798385f.herokuapp.com/https://d36r8jifhgsk5j.cloudfront.net/Willow_TV1080p.m3u8"
         }
-        
-      ]
-    }
-  },
-
-  {
-    "23": {
-      "category": "Cricket",
-      "title": "ICC Women's T20 World Cup",
-      "status": "",
-      "start-in": "07:30 PM | 27-06-2026",
-      "end-in": "11:30 PM | 27-06-2026",
-      "team": [
-        { "team-name": "WI-W", "team-logo": "https://img.sofascore.com/api/v1/team/213120/image" },
-        { "team-name": "IRE-W", "team-logo": "https://img.sofascore.com/api/v1/team/213116/image" }
-      ],
-      "servers": [
-        
         
       ]
     }
