1e50947d180fdb2e4cbf87c98b6c57ed1ab9158a
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-28 20:09:59 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,122 +1,6 @@
 export const liveevent = [
 
   {
-    "29": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "08:00 AM | 28-06-2026",
-      "end-in": "10:10 AM | 28-06-2026",
-      "team": [
-        { "team-name": "Algeria", "team-logo": "https://img.sofascore.com/api/v1/team/4691/image" },
-        { "team-name": "Austria", "team-logo": "https://img.sofascore.com/api/v1/team/4718/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "SOCO",
-          "stream-url": "https://pull.niues.live/live/stream-557845_lhd.m3u8?auth_key=1782615082-0-0-ad9655e87d8fb3f832f32235bc4b2df8"
-        },
-        {
-          "server-name": "CCTV 5+",
-          "stream-url": "https://play.gzxdby.com/live/183334344568_254762755p.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "30": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "08:00 AM | 28-06-2026",
-      "end-in": "10:10 AM | 28-06-2026",
-      "team": [
-        { "team-name": "Argentina", "team-logo": "https://img.sofascore.com/api/v1/team/4819/image" },
-        { "team-name": "Jordan", "team-logo": "https://img.sofascore.com/api/v1/team/4771/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "i Screen",
-          "stream-url": "https://tahmidx.cinemoja2.workers.dev/live/FIFA-2026-4/0/master_2000.m3u8"
-        },
-        {
-          "server-name": "HD FIFA",
-          "stream-url": "https://andro.evrenesoglu57.click/checklist/androstreamliveexn3.m3u8"
-        },
-        {
-          "server-name": "KOORA City",
-          "stream-url": "https://ex.moorarts.xyz/diluoe1_720/index.m3u8"
-        },
-        {
-          "server-name": "CCTV 5",
-          "stream-url": "https://play1.gzxdby.com/live/783234345958_4547667094.m3u8"
-        },
-        {
-          "server-name": "TSN 1",
-          "stream-url": "https://pub-098b280041274cf5929f5e19cfc8ac67.r2.dev/en/index.m3u8"
-        },
-        {
-          "server-name": "S8 TV",
-          "stream-url": "https://hls.lauthaitv.cc/live/jor-arg-wc-nts/index.m3u8"
-        },
-        {
-          "server-name": "TSN 4",
-          "stream-url": "https://ff2srpr7.04334746.net:8443/hls/ulgk1vzsw8aqr.m3u8?s=QDr5q07BUBcCaUgYG9AZLQ&e=1782632875"
-        },
-        {
-          "server-name": "BBC Sport",
-          "stream-url": "https://sltvbd.live/proxy.php?path=hls%2FJCCCC.m3u8&host=http%3A%2F%2F193.47.62.59&ref=http%3A%2F%2Fwww.fawanews.sc%2F"
-        },
-        {
-          "server-name": "OK WIN TV",
-          "stream-url": "https://ivs-play.knozi.top/live/live_1782521674974_nav6zmb6.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://sltvbd.live/proxy.php?path=hls%2Fskkkk.m3u8&host=http%3A%2F%2F193.47.62.55&ref=http%3A%2F%2Fwww.fawanews.sc%2F"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "31": {
-      "category": "Cricket",
-      "title": "BAN in ZIM | Only Test",
-      "status": "",
-      "start-in": "01:30 PM | 28-06-2026",
-      "end-in": "",
-      "team": [
-        { "team-name": "Bangladesh", "team-logo": "https://img.sofascore.com/api/v1/team/187744/image" },
-        { "team-name": "Zimbabwe", "team-logo": "https://img.sofascore.com/api/v1/team/187745/image" }
-      ],
-      "servers": [
-        
-      ]
-    }
-  },
-
-  {
-    "32": {
-      "category": "Cricket",
-      "title": "ICC Women's T20 World Cup",
-      "status": "",
-      "start-in": "03:30 PM | 28-06-2026",
-      "end-in": "07:30 PM | 28-06-2026",
-      "team": [
-        { "team-name": "BAN-W", "team-logo": "https://img.sofascore.com/api/v1/team/187809/image" },
-        { "team-name": "SA-W", "team-logo": "https://img.sofascore.com/api/v1/team/213118/image" }
-      ],
-      "servers": [
-        
-      ]
-    }
-  },
-
-  {
     "33": {
       "category": "Cricket",
       "title": "India in Ireland | 2 T20-I Series",
@@ -128,6 +12,14 @@ export const liveevent = [
         { "team-name": "Ireland", "team-logo": "https://img.sofascore.com/api/v1/team/187766/image" }
       ],
       "servers": [
+        {
+          "server-name": "Willow",
+          "stream-url": "https://sltvbd.live/proxy.php?path=hls%2FFIIIQQQQQQ.m3u8&host=http%3A%2F%2F193.47.62.42&ref=http%3A%2F%2Fwww.fawanews.sc%2F"
+        },
+        {
+          "server-name": "Willow",
+          "stream-url": "https://warm-caverns-48629-92fab798385f.herokuapp.com/https://d36r8jifhgsk5j.cloudfront.net/Willow_TV1080p.m3u8"
+        }
         
       ]
     }
