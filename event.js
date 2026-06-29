53b7324a48abbdd5c125317a88f13838792f2bc7
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-29 22:14:59 +0600

    Update event.js

--- event.js
+++ event.js
@@ -13,6 +13,10 @@ export const liveevent = [
       ],
       "servers": [
         {
+          "server-name": "Special Server",
+          "stream-url": "https://backup.alarafatofficial.workers.dev/?url=https%3A%2F%2Ftc-sg.rockstreamer.com%2Fv1%2F019ed91616121ea540a8171c8e801f%2F019ed92ac80315fc600b1796d4ad8b%2Ftc_sg_1_720p.m3u8&ref=https%3A%2F%2Fiscreen.com.bd%2F&ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F124.0.0.0%20Safari%2F537.36&ekey=SPORTIX_SECURE_KEY_2026"
+        },
+        {
           "server-name": "Bein Sports 1",
           "stream-url": "https://cp11.adabmedia.com/hls2/sport.m3u8?nocache=1782057214460"
         },
@@ -27,6 +31,14 @@ export const liveevent = [
         {
           "server-name": "Servus TV",
           "stream-url": "https://andro.evrenesoglu57.click/checklist/androstreamliveexn3.m3u8"
+        },
+        {
+          "server-name": "TVP Sport",
+          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/89/index.m3u8"
+        },
+        {
+          "server-name": "Sport TV 1",
+          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/33/index.m3u8"
         }
         
       ]
