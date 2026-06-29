120f83eba148a5a0c6322c0ea7e86aaf30fa4359
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-29 22:30:09 +0600

    Update event.js

--- event.js
+++ event.js
@@ -17,8 +17,24 @@ export const liveevent = [
           "stream-url": "https://dfr80qz435crc.cloudfront.net/MNOP/Amagi/Caze/Caze_TV_BR/1080p-vtt/index.m3u8"
         },
         {
+          "server-name": "Bein Max 1",
+          "stream-url": "https://75cup.s3.us-east-2.amazonaws.com/max1/max1_1080p/index.m3u8"
+        },
+        {
+          "server-name": "Bein Max 3",
+          "stream-url": "https://75cup.s3.us-east-2.amazonaws.com/max3/max3_1080p/index.m3u8"
+        },
+        {
+          "server-name": "Ten 1",
+          "stream-url": "https://pub-85819be5176c4d29bb0ce67fa13e15ab.r2.dev/en/index.m3u8"
+        },
+        {
           "server-name": "Special Server",
           "stream-url": "https://backup.alarafatofficial.workers.dev/?url=https%3A%2F%2Ftc-sg.rockstreamer.com%2Fv1%2F019ed91616121ea540a8171c8e801f%2F019ed92ac80315fc600b1796d4ad8b%2Ftc_sg_1_720p.m3u8&ref=https%3A%2F%2Fiscreen.com.bd%2F&ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F124.0.0.0%20Safari%2F537.36&ekey=SPORTIX_SECURE_KEY_2026"
+        },
+        {
+          "server-name": "DAZN",
+          "stream-url": "https://pub-85819be5176c4d29bb0ce67fa13e15ab.r2.dev/sp/index.m3u8"
         },
         {
           "server-name": "Bein Sports 1",
