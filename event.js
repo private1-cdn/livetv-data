7684a8ac4165ff38da060cacee2cdca1a2243c6c
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-06-30 18:00:01 +0600

    Update event.js

--- event.js
+++ event.js
@@ -4,66 +4,14 @@ export const liveevent = [
     "36": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
-      "status": "Finished",
-      "start-in": "11:00 PM | 29-06-2026",
-      "end-in": "",
+      "status": "",
+      "start-in": "11:00 PM | 30-06-2026",
+      "end-in": "02:00 AM | 01-07-2026",
       "team": [
-        { "team-name": "Brazil", "team-logo": "https://img.sofascore.com/api/v1/team/4748/image" },
-        { "team-name": "Japan", "team-logo": "https://img.sofascore.com/api/v1/team/4770/image" }
+        { "team-name": "Côte d'Ivoire", "team-logo": "https://img.sofascore.com/api/v1/team/4768/image" },
+        { "team-name": "Norway", "team-logo": "https://img.sofascore.com/api/v1/team/4475/image" }
       ],
       "servers": [
-        {
-          "server-name": "Tofee",
-          "stream-url": "https://proximitybd-mominrocks.lovable.app/api/proxy?u=https%3A%2F%2Fprod-cdn01-live.toffeelive.com%2Flive%2FFIFA-2026%2Fsst%2F0%2Fmaster_1750.m3u8%3Fhdntl%3DExpires%3D1782836406%7E_GO%3DGenerated%7EURLPrefix%3DaHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20%7ESignature%3DAeQsclB4bRTNzFG_TDhbZgaVQpFj8uu8TAM6-ljhmuPKJdvkfan-BpTwYzRttRsPyhs-A3GeFscfBnOl_WXGjlWyIHUA"
-        },
-        {
-          "server-name": "Bioscope",
-          "stream-url": "https://d1kep4defqj6r1.cloudfront.net/out/v1/4e21d5a122c549289dd6feab259266e8/index_1.m3u8"
-        },
-        {
-          "server-name": "Specila Server 1",
-          "stream-url": "https://tahmidx.cinemoja.workers.dev"
-        },
-        {
-          "server-name": "Special Server 2",
-          "stream-url": "https://tahmidx.shusanta-project.workers.dev"
-        },
-        {
-          "server-name": "Special Server 3",
-          "stream-url": "https://backup.alarafatofficial.workers.dev/?url=https%3A%2F%2Ftc-sg.rockstreamer.com%2Fv1%2F019ed91616121ea540a8171c8e801f%2F019ed92ac80315fc600b1796d4ad8b%2Ftc_sg_1_720p.m3u8&ref=https%3A%2F%2Fiscreen.com.bd%2F&ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F124.0.0.0%20Safari%2F537.36&ekey=SPORTIX_SECURE_KEY_2026"
-        },
-        {
-          "server-name": "DAZN",
-          "stream-url": "https://pub-85819be5176c4d29bb0ce67fa13e15ab.r2.dev/sp/index.m3u8"
-        },
-        {
-          "server-name": "Somoy TV",
-          "stream-url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://cp11.adabmedia.com/hls2/sport.m3u8?nocache=1782057214460"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
-        },
-        {
-          "server-name": "Servus TV",
-          "stream-url": "https://andro.evrenesoglu57.click/checklist/androstreamliveexn3.m3u8"
-        },
-        {
-          "server-name": "TVP Sport",
-          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/89/index.m3u8"
-        },
-        {
-          "server-name": "Sport TV 1",
-          "stream-url": "https://1nyaler.streamhostingcdn.top/stream/33/index.m3u8"
-        },
-        {
-          "server-name": "FOX",
-          "stream-url": "http://84.17.50.102/fox/index.m3u8"
-        }
         
       ]
     }
