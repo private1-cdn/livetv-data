a663910e2ff27dcad1e844abe1afdbbd5763696a
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-03 09:27:37 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,68 +1,15 @@
 export const liveevent = [
 
   {
-    "42": {
+    "44": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
       "status": "",
-      "start-in": "01:00 AM | 03-07-2026",
-      "end-in": "04:00 AM | 03-07-2026",
+      "start-in": "09:00 AM | 03-07-2026",
+      "end-in": "11:59 AM | 03-07-2026",
       "team": [
-        { "team-name": "Spain", "team-logo": "https://img.sofascore.com/api/v1/team/4698/image" },
-        { "team-name": "Austria", "team-logo": "https://img.sofascore.com/api/v1/team/4718/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "i Screen",
-          "stream-url": "https://cricxtv.fun/isceen.php?file=https://tc-sg.rockstreamer.com/v1/019ed91616121ea540a8171c8e801f/019ed92ac80315fc600b1796d4ad8b/tc_sg_1_1080p.m3u8"
-        },
-        {
-          "server-name": "Tofee",
-          "stream-url": "https://rajutv.pages.dev/proxy?url=https://prod-cdn01-live.toffeelive.com/live/FIFA-2026/sst/0/master_1500.m3u8?hdntl=Expires=1783102977~_GO=Generated~URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20~Signature=AeQsclCnnN1PG78aLBVbamllNib5MA13mmD9YNe7jP0fPipq04qTkLzvdawumBgGHTn0ghGI36WibGA_kfLvSDo2Q38G"
-        },
-        {
-          "server-name": "FOX",
-          "stream-url": "https://sltvbd.live/proxy.php?path=hls%2FTHXCZzsssQ.m3u8&host=http%3A%2F%2F193.47.62.59&ref=http%3A%2F%2Fwww.fawanews.sc%2F"
-        },
-        {
-          "server-name": "Soco",
-          "stream-url": "https://pull.niur.live/live/stream-327159_lhd.m3u8?txSecret=afdb1e8fd1de86dedc0f85b47e8a1ab1&txTime=6a470713"
-        },
-        {
-          "server-name": "Soco",
-          "stream-url": "https://pull.niur.live/live/stream-9912116_lsd.m3u8?txSecret=d245f1c0a73d61f04a7569de7c0c4a6a&txTime=6a470623"
-        },
-        {
-          "server-name": "KooraX90",
-          "stream-url": "https://1.simoplay.com/my-hls/h9asfma10d5.m3u8"
-        },
-        {
-          "server-name": "Somoy TV",
-          "stream-url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://cp11.adabmedia.com/hls2/sport.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
-    "43": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "05:00 AM | 03-07-2026",
-      "end-in": "08:00 AM | 03-07-2026",
-      "team": [
-        { "team-name": "Portugal", "team-logo": "https://img.sofascore.com/api/v1/team/4704/image" },
-        { "team-name": "Croatia", "team-logo": "https://img.sofascore.com/api/v1/team/4715/image" }
+        { "team-name": "Switzerland", "team-logo": "https://img.sofascore.com/api/v1/team/4699/image" },
+        { "team-name": "Algeria", "team-logo": "https://img.sofascore.com/api/v1/team/4691/image" }
       ],
       "servers": [
         {
@@ -80,18 +27,6 @@ export const liveevent = [
           "stream-url": "https://sltvbd.live/proxy.php?path=hls%2FTHXCZzsssQ.m3u8&host=http%3A%2F%2F193.47.62.59&ref=http%3A%2F%2Fwww.fawanews.sc%2F"
         },
         {
-          "server-name": "Soco",
-          "stream-url": "https://pull.niur.live/live/stream-327159_lhd.m3u8?txSecret=afdb1e8fd1de86dedc0f85b47e8a1ab1&txTime=6a470713"
-        },
-        {
-          "server-name": "Soco",
-          "stream-url": "https://pull.niur.live/live/stream-9912116_lsd.m3u8?txSecret=d245f1c0a73d61f04a7569de7c0c4a6a&txTime=6a470623"
-        },
-        {
-          "server-name": "KooraX90",
-          "stream-url": "https://1.simoplay.com/my-hls/h9asfma10d5.m3u8"
-        },
-        {
           "server-name": "Somoy TV",
           "stream-url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8"
         },
@@ -103,23 +38,6 @@ export const liveevent = [
           "server-name": "Bein Sports 1",
           "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
         }
-        
-      ]
-    }
-  },
-
-  {
-    "44": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "09:00 AM | 03-07-2026",
-      "end-in": "11:59 AM | 03-07-2026",
-      "team": [
-        { "team-name": "Switzerland", "team-logo": "https://img.sofascore.com/api/v1/team/4699/image" },
-        { "team-name": "Algeria", "team-logo": "https://img.sofascore.com/api/v1/team/4691/image" }
-      ],
-      "servers": [
         
       ]
     }
