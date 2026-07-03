b55d3a90fb25967f1a0f85c77ca609525fca48db
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-04 03:31:28 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,17 +1,23 @@
 export const liveevent = [
 
   {
-    "45": {
+    "46": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
       "status": "",
-      "start-in": "12:00 AM | 04-07-2026",
-      "end-in": "03:00 AM | 04-07-2026",
+      "start-in": "04:00 AM | 04-07-2026",
+      "end-in": "07:00 AM | 04-07-2026",
       "team": [
-        { "team-name": "Australia", "team-logo": "https://img.sofascore.com/api/v1/team/4741/image" },
-        { "team-name": "Egypt", "team-logo": "https://img.sofascore.com/api/v1/team/4758/image" }
+        { "team-name": "Argentina", "team-logo": "https://img.sofascore.com/api/v1/team/4819/image" },
+        { "team-name": "Cape Verde", "team-logo": "https://img.sofascore.com/api/v1/team/4753/image" }
       ],
       "servers": [
+        {
+          "server-name": "FOX - FHD",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
+          "keyid": "f6564ec2aee819046328a0e153be574d",
+          "key": "ff46a8a1031eb27ef22576a077c98ab7"
+        },
         {
           "server-name": "WC TV",
           "mpd-stream-url": "https://qp-pldt-live-bpk-ucd-prod.akamaized.net/bpk-tv/fifa_ppv1/default/index.mpd",
@@ -23,12 +29,6 @@ export const liveevent = [
           "mpd-stream-url": "https://dmbgaz4xf6jyh.cloudfront.net/live-stream-dai/69690cfcce0dc031830e7c1a/123a264b71764485bd49eca2d552cf33/d97743de3620a865848fff7a9de14dda1bf2409d/session/ecb260d1-eb4b-4a93-8f14-32bb03b69853/manifest.mpd?aid=688aae6d61c87d6274c05065&dnt=true&access_token=63ey9SOpQeHBRfWJYQLXQjelQc0QM1HXmlHp4iZrH8UCOCM8QtGIGg1sEEPJH0R0yXktUEXdLeB&uid=Knx65GY6UbaWSADZybEvf6m5MvqNIuNs&sid=bwRy9BgSc4RFeVhYh52hVIY8uPsJ4v4d&pid=A0xT1VnepBvKKqyeyRNtNkZBmHBrNNZM&ref=ditu.caracoltv.com&ext_pb=0&es=dmbgaz4xf6jyh.cloudfront.net&proto=https&pz=us&xr=us-east-2",
           "keyid": "1c6bfd8d1cdd4df69ea5fc4dd2f025bb",
           "key": "b6fd90027dac7a94a9461f8b2638cbb4"
-        },
-        {
-          "server-name": "FOX - FHD",
-          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
-          "keyid": "f6564ec2aee819046328a0e153be574d",
-          "key": "ff46a8a1031eb27ef22576a077c98ab7"
         },
         {
           "server-name": "i Screen",
@@ -46,23 +46,6 @@ export const liveevent = [
           "server-name": "Bein Sports 1",
           "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
         }
-        
-      ]
-    }
-  },
-
-  {
-    "46": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "04:00 AM | 04-07-2026",
-      "end-in": "07:00 AM | 04-07-2026",
-      "team": [
-        { "team-name": "Argentina", "team-logo": "https://img.sofascore.com/api/v1/team/4819/image" },
-        { "team-name": "Cape Verde", "team-logo": "https://img.sofascore.com/api/v1/team/4753/image" }
-      ],
-      "servers": [
         
       ]
     }
