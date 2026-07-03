5c7cbc15fabeb984749f399fc2e5f9697c7e1cdc
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-03 23:50:28 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,17 +1,23 @@
 export const liveevent = [
 
   {
-    "44": {
+    "45": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
       "status": "",
-      "start-in": "09:00 AM | 03-07-2026",
-      "end-in": "11:59 AM | 03-07-2026",
+      "start-in": "12:00 AM | 04-07-2026",
+      "end-in": "03:00 AM | 04-07-2026",
       "team": [
-        { "team-name": "Switzerland", "team-logo": "https://img.sofascore.com/api/v1/team/4699/image" },
-        { "team-name": "Algeria", "team-logo": "https://img.sofascore.com/api/v1/team/4691/image" }
+        { "team-name": "Australia", "team-logo": "https://img.sofascore.com/api/v1/team/4741/image" },
+        { "team-name": "Egypt", "team-logo": "https://img.sofascore.com/api/v1/team/4758/image" }
       ],
       "servers": [
+        {
+          "server-name": "WC TV",
+          "mpd-stream-url": "https://qp-pldt-live-bpk-ucd-prod.akamaized.net/bpk-tv/fifa_ppv1/default/index.mpd",
+          "keyid": "2c338a117d434ce4bbe3569231af90f1",
+          "key": "a9633d901ee8a3f4f58ac314b5c5f4fb"
+        },
         {
           "server-name": "GOL TV VIVO",
           "mpd-stream-url": "https://dmbgaz4xf6jyh.cloudfront.net/live-stream-dai/69690cfcce0dc031830e7c1a/123a264b71764485bd49eca2d552cf33/d97743de3620a865848fff7a9de14dda1bf2409d/session/ecb260d1-eb4b-4a93-8f14-32bb03b69853/manifest.mpd?aid=688aae6d61c87d6274c05065&dnt=true&access_token=63ey9SOpQeHBRfWJYQLXQjelQc0QM1HXmlHp4iZrH8UCOCM8QtGIGg1sEEPJH0R0yXktUEXdLeB&uid=Knx65GY6UbaWSADZybEvf6m5MvqNIuNs&sid=bwRy9BgSc4RFeVhYh52hVIY8uPsJ4v4d&pid=A0xT1VnepBvKKqyeyRNtNkZBmHBrNNZM&ref=ditu.caracoltv.com&ext_pb=0&es=dmbgaz4xf6jyh.cloudfront.net&proto=https&pz=us&xr=us-east-2",
@@ -19,12 +25,14 @@ export const liveevent = [
           "key": "b6fd90027dac7a94a9461f8b2638cbb4"
         },
         {
-          "server-name": "i Screen",
-          "stream-url": "https://cricxtv.fun/isceen.php?file=https://tc-sg.rockstreamer.com/v1/019ed91616121ea540a8171c8e801f/019ed92ac80315fc600b1796d4ad8b/tc_sg_1_1080p.m3u8"
+          "server-name": "FOX - FHD",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
+          "keyid": "f6564ec2aee819046328a0e153be574d",
+          "key": "ff46a8a1031eb27ef22576a077c98ab7"
         },
         {
-          "server-name": "Special",
-          "stream-url": "https://sltvbd.live/proxy.php?path=hls%2FTHXCZzsssQ.m3u8&host=http%3A%2F%2F193.47.62.59&ref=http%3A%2F%2Fwww.fawanews.sc%2F"
+          "server-name": "i Screen",
+          "stream-url": "https://cricxtv.fun/isceen.php?file=https://tc-sg.rockstreamer.com/v1/019ed91616121ea540a8171c8e801f/019ed92ac80315fc600b1796d4ad8b/tc_sg_1_1080p.m3u8"
         },
         {
           "server-name": "Somoy TV",
@@ -38,23 +46,6 @@ export const liveevent = [
           "server-name": "Bein Sports 1",
           "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
         }
-        
-      ]
-    }
-  },
-
-  {
-    "45": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "12:00 AM | 04-07-2026",
-      "end-in": "03:00 AM | 04-07-2026",
-      "team": [
-        { "team-name": "Australia", "team-logo": "https://img.sofascore.com/api/v1/team/4741/image" },
-        { "team-name": "Egypt", "team-logo": "https://img.sofascore.com/api/v1/team/4758/image" }
-      ],
-      "servers": [
         
       ]
     }
