8469a0bb8a43915e9b91d8c380bdd1ba8b8d3d75
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-04 07:47:32 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,75 +1,6 @@
 export const liveevent = [
 
   {
-    "46": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "04:00 AM | 04-07-2026",
-      "end-in": "07:00 AM | 04-07-2026",
-      "team": [
-        { "team-name": "Argentina", "team-logo": "https://img.sofascore.com/api/v1/team/4819/image" },
-        { "team-name": "Cape Verde", "team-logo": "https://img.sofascore.com/api/v1/team/4753/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "FOX - FHD",
-          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
-          "keyid": "f6564ec2aee819046328a0e153be574d",
-          "key": "ff46a8a1031eb27ef22576a077c98ab7"
-        },
-        {
-          "server-name": "TSN 4",
-          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ihys8nw4wv/out/v1/fde190f369484bc6b6117cc16cd82a9f/cenc.mpd",
-          "keyid": "abc5b2883121012850ebda05b528c5ec",
-          "key": "e5250924f4b738905f7163a0134587a7"
-        },
-        {
-          "server-name": "ITV 1",
-          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/0eiyyz8qzm/out/v1/dd17af8835fe4bd087d1a4e359b635d7/cenc.mpd",
-          "keyid": "30089c52924f037b225b82c616fee2a5",
-          "key": "f55dc8b66ed4fc6753d6035ae7e17144"
-        },
-        {
-          "server-name": "WC TV",
-          "mpd-stream-url": "https://qp-pldt-live-bpk-ucd-prod.akamaized.net/bpk-tv/fifa_ppv1/default/index.mpd",
-          "keyid": "2c338a117d434ce4bbe3569231af90f1",
-          "key": "a9633d901ee8a3f4f58ac314b5c5f4fb"
-        },
-        {
-          "server-name": "CT Sport",
-          "mpd-stream-url": "https://dash2.antik.sk/stream/nvidia_ct_sport/playlist_cenc.mpd",
-          "keyid": "11223344556677889900112233445566",
-          "key": "4b80724d0ef86bcb2c21f7999d67739d"
-        },
-        {
-          "server-name": "GOL TV VIVO",
-          "mpd-stream-url": "https://dmbgaz4xf6jyh.cloudfront.net/live-stream-dai/69690cfcce0dc031830e7c1a/123a264b71764485bd49eca2d552cf33/d97743de3620a865848fff7a9de14dda1bf2409d/session/ecb260d1-eb4b-4a93-8f14-32bb03b69853/manifest.mpd?aid=688aae6d61c87d6274c05065&dnt=true&access_token=63ey9SOpQeHBRfWJYQLXQjelQc0QM1HXmlHp4iZrH8UCOCM8QtGIGg1sEEPJH0R0yXktUEXdLeB&uid=Knx65GY6UbaWSADZybEvf6m5MvqNIuNs&sid=bwRy9BgSc4RFeVhYh52hVIY8uPsJ4v4d&pid=A0xT1VnepBvKKqyeyRNtNkZBmHBrNNZM&ref=ditu.caracoltv.com&ext_pb=0&es=dmbgaz4xf6jyh.cloudfront.net&proto=https&pz=us&xr=us-east-2",
-          "keyid": "1c6bfd8d1cdd4df69ea5fc4dd2f025bb",
-          "key": "b6fd90027dac7a94a9461f8b2638cbb4"
-        },
-        {
-          "server-name": "i Screen",
-          "stream-url": "https://cricxtv.fun/isceen.php?file=https://tc-sg.rockstreamer.com/v1/019ed91616121ea540a8171c8e801f/019ed92ac80315fc600b1796d4ad8b/tc_sg_1_1080p.m3u8"
-        },
-        {
-          "server-name": "Somoy TV",
-          "stream-url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://cp11.adabmedia.com/hls2/sport.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
-        }
-        
-      ]
-    }
-  },
-
-  {
     "47": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
@@ -106,12 +37,6 @@ export const liveevent = [
           "key": "a9633d901ee8a3f4f58ac314b5c5f4fb"
         },
         {
-          "server-name": "CT Sport",
-          "mpd-stream-url": "https://dash2.antik.sk/stream/nvidia_ct_sport/playlist_cenc.mpd",
-          "keyid": "11223344556677889900112233445566",
-          "key": "4b80724d0ef86bcb2c21f7999d67739d"
-        },
-        {
           "server-name": "GOL TV VIVO",
           "mpd-stream-url": "https://dmbgaz4xf6jyh.cloudfront.net/live-stream-dai/69690cfcce0dc031830e7c1a/123a264b71764485bd49eca2d552cf33/d97743de3620a865848fff7a9de14dda1bf2409d/session/ecb260d1-eb4b-4a93-8f14-32bb03b69853/manifest.mpd?aid=688aae6d61c87d6274c05065&dnt=true&access_token=63ey9SOpQeHBRfWJYQLXQjelQc0QM1HXmlHp4iZrH8UCOCM8QtGIGg1sEEPJH0R0yXktUEXdLeB&uid=Knx65GY6UbaWSADZybEvf6m5MvqNIuNs&sid=bwRy9BgSc4RFeVhYh52hVIY8uPsJ4v4d&pid=A0xT1VnepBvKKqyeyRNtNkZBmHBrNNZM&ref=ditu.caracoltv.com&ext_pb=0&es=dmbgaz4xf6jyh.cloudfront.net&proto=https&pz=us&xr=us-east-2",
           "keyid": "1c6bfd8d1cdd4df69ea5fc4dd2f025bb",
@@ -124,14 +49,6 @@ export const liveevent = [
         {
           "server-name": "Somoy TV",
           "stream-url": "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://cp11.adabmedia.com/hls2/sport.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 1",
-          "stream-url": "https://edge22.776740.ir.cdn.ir/hls2/sport.m3u8"
         }
         
       ]
