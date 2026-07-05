1ba65ced55265dbef5d7f65b3ffff07a5c89d753
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-05 20:52:24 +0600

    Update event.js

--- event.js
+++ event.js
@@ -31,6 +31,12 @@ export const liveevent = [
           "key": "f55dc8b66ed4fc6753d6035ae7e17144"
         },
         {
+          "server-name": "WC TV",
+          "mpd-stream-url": "https://qp-pldt-live-bpk-ucd-prod.akamaized.net/bpk-tv/fifa_ppv1/default/index.mpd",
+          "keyid": "2c338a117d434ce4bbe3569231af90f1",
+          "key": "a9633d901ee8a3f4f58ac314b5c5f4fb"
+        },
+        {
           "server-name": "GOL TV VIVO",
           "mpd-stream-url": "https://dmbgaz4xf6jyh.cloudfront.net/live-stream-dai/69690cfcce0dc031830e7c1a/123a264b71764485bd49eca2d552cf33/d97743de3620a865848fff7a9de14dda1bf2409d/session/ecb260d1-eb4b-4a93-8f14-32bb03b69853/manifest.mpd?aid=688aae6d61c87d6274c05065&dnt=true&access_token=63ey9SOpQeHBRfWJYQLXQjelQc0QM1HXmlHp4iZrH8UCOCM8QtGIGg1sEEPJH0R0yXktUEXdLeB&uid=Knx65GY6UbaWSADZybEvf6m5MvqNIuNs&sid=bwRy9BgSc4RFeVhYh52hVIY8uPsJ4v4d&pid=A0xT1VnepBvKKqyeyRNtNkZBmHBrNNZM&ref=ditu.caracoltv.com&ext_pb=0&es=dmbgaz4xf6jyh.cloudfront.net&proto=https&pz=us&xr=us-east-2",
           "keyid": "1c6bfd8d1cdd4df69ea5fc4dd2f025bb",
