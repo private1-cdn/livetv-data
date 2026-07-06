d92468f25b4123dfe974bb0be23f5bb614b6a898
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-06 19:52:46 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,15 +1,15 @@
 export const liveevent = [
 
   {
-    "50": {
+    "52": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
       "status": "",
-      "start-in": "02:00 AM | 06-07-2026",
-      "end-in": "05:00 AM | 06-07-2026",
+      "start-in": "01:00 AM | 07-07-2026",
+      "end-in": "04:00 AM | 07-07-2026",
       "team": [
-        { "team-name": "Brazil", "team-logo": "https://img.sofascore.com/api/v1/team/4748/image" },
-        { "team-name": "Norway", "team-logo": "https://img.sofascore.com/api/v1/team/4475/image" }
+        { "team-name": "Portugal", "team-logo": "https://img.sofascore.com/api/v1/team/4704/image" },
+        { "team-name": "Spain", "team-logo": "https://img.sofascore.com/api/v1/team/4698/image" }
       ],
       "servers": [
         {
@@ -78,41 +78,6 @@ export const liveevent = [
           "server-name": "Telemundo",
           "stream-url": "https://tda-nnaa-drct-pa6dy.fast.nbcuni.com/live/master_2.m3u8"
         }
-        
-      ]
-    }
-  },
-
-  {
-    "51": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "06:00 AM | 06-07-2026",
-      "end-in": "09:00 AM | 06-07-2026",
-      "team": [
-        { "team-name": "Mexico", "team-logo": "https://img.sofascore.com/api/v1/team/4781/image" },
-        { "team-name": "England", "team-logo": "https://img.sofascore.com/api/v1/team/4713/image" }
-      ],
-      "servers": [
-        
-      ]
-    }
-  },
-
-  {
-    "52": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "01:00 AM | 07-07-2026",
-      "end-in": "04:00 AM | 07-07-2026",
-      "team": [
-        { "team-name": "Portugal", "team-logo": "https://img.sofascore.com/api/v1/team/4704/image" },
-        { "team-name": "Spain", "team-logo": "https://img.sofascore.com/api/v1/team/4698/image" }
-      ],
-      "servers": [
-        
       ]
     }
   },
@@ -129,7 +94,72 @@ export const liveevent = [
         { "team-name": "Belgium", "team-logo": "https://img.sofascore.com/api/v1/team/4717/image" }
       ],
       "servers": [
-        
+        {
+          "server-name": "FOX - FHD",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
+          "keyid": "f6564ec2aee819046328a0e153be574d",
+          "key": "ff46a8a1031eb27ef22576a077c98ab7"
+        },
+        {
+          "server-name": "TSN 1",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd",
+          "keyid": "14eeabf30c14b7fbf3008c03099ce011",
+          "key": "17d2ac8dbc5429bd70af3433aa12158d"
+        },
+        {
+          "server-name": "TSN 4",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/ihys8nw4wv/out/v1/fde190f369484bc6b6117cc16cd82a9f/cenc.mpd",
+          "keyid": "abc5b2883121012850ebda05b528c5ec",
+          "key": "e5250924f4b738905f7163a0134587a7"
+        },
+        {
+          "server-name": "D Sports",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/3gg2jnixjn/out/v1/e1840e01f3f14563b66bbb944d5cc54c/cenc.mpd",
+          "keyid": "f8b207c10f3f76aeba32a360ec52b9e4",
+          "key": "afad49d20eb39670e93e371c1d669921"
+        },
+        {
+          "server-name": "ITV 1",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/0eiyyz8qzm/out/v1/dd17af8835fe4bd087d1a4e359b635d7/cenc.mpd",
+          "keyid": "30089c52924f037b225b82c616fee2a5",
+          "key": "f55dc8b66ed4fc6753d6035ae7e17144"
+        },
+        {
+          "server-name": "WC TV",
+          "mpd-stream-url": "https://qp-pldt-live-bpk-ucd-prod.akamaized.net/bpk-tv/fifa_ppv1/default/index.mpd",
+          "keyid": "2c338a117d434ce4bbe3569231af90f1",
+          "key": "a9633d901ee8a3f4f58ac314b5c5f4fb"
+        },
+        {
+          "server-name": "GOL TV VIVO",
+          "mpd-stream-url": "https://dmbgaz4xf6jyh.cloudfront.net/live-stream-dai/69690cfcce0dc031830e7c1a/123a264b71764485bd49eca2d552cf33/d97743de3620a865848fff7a9de14dda1bf2409d/session/ecb260d1-eb4b-4a93-8f14-32bb03b69853/manifest.mpd?aid=688aae6d61c87d6274c05065&dnt=true&access_token=63ey9SOpQeHBRfWJYQLXQjelQc0QM1HXmlHp4iZrH8UCOCM8QtGIGg1sEEPJH0R0yXktUEXdLeB&uid=Knx65GY6UbaWSADZybEvf6m5MvqNIuNs&sid=bwRy9BgSc4RFeVhYh52hVIY8uPsJ4v4d&pid=A0xT1VnepBvKKqyeyRNtNkZBmHBrNNZM&ref=ditu.caracoltv.com&ext_pb=0&es=dmbgaz4xf6jyh.cloudfront.net&proto=https&pz=us&xr=us-east-2",
+          "keyid": "1c6bfd8d1cdd4df69ea5fc4dd2f025bb",
+          "key": "b6fd90027dac7a94a9461f8b2638cbb4"
+        },
+        {
+          "server-name": "CAZE TV",
+          "stream-url": "https://dfr80qz435crc.cloudfront.net/MNOP/Amagi/Caze/Caze_TV_BR/1080p-vtt/index.m3u8"
+        },
+        {
+          "server-name": "Bioscope",
+          "stream-url": "https://hls.diptom3ow.workers.dev/r/https%3A%2F%2Ffifa-stream-01.bioscopelive.com%2Fout%2Fv1%2F60e7608404004b1186261497b404630b%2Findex_2.m3u8"
+        },
+        {
+          "server-name": "T Sports",
+          "stream-url": "https://flussonic.deltainfonet.com/01_Tsports_HD/tracks-v1a1/mono.m3u8"
+        },
+        {
+          "server-name": "Bein Sports 5",
+          "stream-url": "https://node8.no-612.workers.dev/resource/MjQwNTk5MDY0MTY5OjI0MDU5OTA2NDE2OToxMjAzNTpsaXZl/index"
+        },
+        {
+          "server-name": "FOX",
+          "stream-url": "https://d1jzu95oc8fgt3.cloudfront.net/FOX_Sports1080p.m3u8"
+        },
+        {
+          "server-name": "Telemundo",
+          "stream-url": "https://tda-nnaa-drct-pa6dy.fast.nbcuni.com/live/master_2.m3u8"
+        }
       ]
     }
   },
