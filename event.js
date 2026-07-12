494484bdd693f4b2f89d27a38255fc17b0a7b820
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-12 09:48:46 +0600

    Update event.js

--- event.js
+++ event.js
@@ -1,46 +1,25 @@
 export const liveevent = [
 
   {
-    "59": {
-      "category": "Football",
-      "title": "Fifa World Cup 2026",
-      "status": "",
-      "start-in": "07:00 AM | 12-07-2026",
-      "end-in": "",
-      "team": [
-        { "team-name": "Argentina", "team-logo": "https://img.sofascore.com/api/v1/team/4819/image" },
-        { "team-name": "Switzerland", "team-logo": "https://img.sofascore.com/api/v1/team/4699/image" }
-      ],
-      "servers": [
-        {
-          "server-name": "Soco",
-          "stream-url": "https://pull.niues.live/live/stream-9912060_lhd.m3u8?auth_key=1783284383-0-0-ba541e77b9330eab42075af6aa5029e4"
-        },
-        {
-          "server-name": "Bioscope",
-          "stream-url": "https://hls.diptom3ow.workers.dev/r/https%3A%2F%2Ffifa-stream-01.bioscopelive.com%2Fout%2Fv1%2F60e7608404004b1186261497b404630b%2Findex_2.m3u8"
-        },
-        {
-          "server-name": "Bein Sports 5",
-          "stream-url": "https://node8.no-612.workers.dev/resource/MjQwNTk5MDY0MTY5OjI0MDU5OTA2NDE2OToxMjAzNTpsaXZl/index"
-        }
-        
-      ]
-    }
-  },
-
-  {
     "60": {
       "category": "Football",
       "title": "Fifa World Cup 2026",
       "status": "",
       "start-in": "01:00 AM | 15-07-2026",
-      "end-in": "",
+      "end-in": "04:00 AM | 15-07-2026",
       "team": [
-        { "team-name": "W97", "team-logo": "#" },
-        { "team-name": "W98", "team-logo": "#" }
+        { "team-name": "France", "team-logo": "https://img.sofascore.com/api/v1/team/4481/image" },
+        { "team-name": "Spain", "team-logo": "https://img.sofascore.com/api/v1/team/4698/image" }
       ],
       "servers": [
+        {
+          "server-name": "Bioscope",
+          "stream-url": "#"
+        },
+        {
+          "server-name": "Bein Sports 5",
+          "stream-url": "https://node8.no-612.workers.dev/resource/MjQwNTk5MDY0MTY5OjI0MDU5OTA2NDE2OToxMjAzNTpsaXZl/index"
+        }
         
       ]
     }
@@ -54,7 +33,7 @@ export const liveevent = [
       "start-in": "01:00 AM | 16-07-2026",
       "end-in": "",
       "team": [
-        { "team-name": "W99", "team-logo": "#" },
+        { "team-name": "England", "team-logo": "https://img.sofascore.com/api/v1/team/4713/image" },
         { "team-name": "W100", "team-logo": "#" }
       ],
       "servers": [
