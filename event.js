143e44ab2e599b0cbb286b389c746e96e0c9ecf4
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-15 00:52:56 +0600

    Update event.js

--- event.js
+++ event.js
@@ -5,7 +5,7 @@ export const liveevent = [
       "category": "Football",
       "title": "Fifa World Cup 2026",
       "status": "",
-      "start-in": "01:00 AM | 15-07-2026",
+      "start-in": "12:30 AM | 15-07-2026",
       "end-in": "04:00 AM | 15-07-2026",
       "team": [
         { "team-name": "France", "team-logo": "https://img.sofascore.com/api/v1/team/4481/image" },
