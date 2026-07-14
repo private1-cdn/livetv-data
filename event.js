52b5ee74c56b947114eec80c9986ecacaa8c3e31
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-14 23:26:22 +0600

    Update event.js

--- event.js
+++ event.js
@@ -13,10 +13,34 @@ export const liveevent = [
       ],
       "servers": [
         {
+          "server-name": "FOX",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
+          "keyid": "f6564ec2aee819046328a0e153be574d",
+          "key": "ff46a8a1031eb27ef22576a077c98ab7"
+        },
+        {
+          "server-name": "TSN 1",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd",
+          "keyid": "14eeabf30c14b7fbf3008c03099ce011",
+          "key": "17d2ac8dbc5429bd70af3433aa12158d"
+        },
+        {
+          "server-name": "TSN 2",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/cjglydxghe/out/v1/8977baf175da4b94873194613dd3fe55/cenc.mpd",
+          "keyid": "85b277daf5aae05833fe43a68f587968",
+          "key": "d52d7e9bc0bcd98787efd547ac91eca0"
+        },
+        {
           "server-name": "TSN 5",
           "mpd-stream-url": "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/cscevwljkq/out/v1/972185041b244140860b7d56398e9aaf/cenc.mpd",
           "keyid": "385ceb9714b75e0cef61254f80b31002",
           "key": "18dce92a2891fee68d21ede5173230f8"
+        },
+        {
+          "server-name": "D Sports",
+          "mpd-stream-url": "https://otte.cache.aiv-cdn.net/iad-nitro/live/clients/dash/enc/z5oyxzsxdk/out/v1/7695a0f64a0e424b973d5b09a2a3eb91/cenc.mpd",
+          "keyid": "f836853d8eac19446ed9535f5fc568b1",
+          "key": "b3bc5ef00602b29abac7e482d3d9fbf3"
         },
         {
           "server-name": "Unite8 Sports 2",
