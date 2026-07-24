1399e80ccde7bd770b8c7db8be0d71e777e0b45e
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-24 10:51:57 +0600

    Update json.js

--- Test-Video/json.js
+++ Test-Video/json.js
@@ -1,6 +1,34 @@
 
 export const video = [
   {
+    id: 5,
+    category: "Bangla",
+    title: "Favorite Video Number 5",
+    thumbnail: "#",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 4,
+    category: "Bangla",
+    title: "Favorite Video Number 4",
+    thumbnail: "#",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 3,
+    category: "Bangla",
+    title: "Favorite Video Number 3",
+    thumbnail: "#",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 2,
+    category: "Bangla",
+    title: "Favorite Video Number 2",
+    thumbnail: "#",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
     id: 1,
     category: "Bangla",
     title: "Favorite Video Number 1",
