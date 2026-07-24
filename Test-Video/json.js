d0fd8adfd9b089eb254ce646549f2d0d2359af3a
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-24 10:57:40 +0600

    Update json.js

--- Test-Video/json.js
+++ Test-Video/json.js
@@ -4,35 +4,35 @@ export const video = [
     id: 5,
     category: "Bangla",
     title: "Favorite Video Number 5",
-    thumbnail: "#",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
   },
   {
     id: 4,
-    category: "Bangla",
+    category: "Indian",
     title: "Favorite Video Number 4",
-    thumbnail: "#",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
   },
 {
     id: 3,
     category: "Bangla",
     title: "Favorite Video Number 3",
-    thumbnail: "#",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
   },
 {
     id: 2,
-    category: "Bangla",
+    category: "Indian",
     title: "Favorite Video Number 2",
-    thumbnail: "#",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
   },
   {
     id: 1,
     category: "Bangla",
     title: "Favorite Video Number 1",
-    thumbnail: "#",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
   }
 ];
