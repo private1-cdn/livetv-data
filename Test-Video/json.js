a2bed0767e9e5329729ee06c7ff746eee133ba65
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-07-24 10:46:43 +0600

    Update and rename dsvaebe.md to json.js

---
+++ Test-Video/json.js
@@ -0,0 +1,10 @@
+
+export const video = [
+  {
+    id: 1,
+    category: "Bangla",
+    title: "Favorite Video Number 1",
+    thumbnail: "#",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  }
+];
