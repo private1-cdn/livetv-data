14a626ae4aee6c262d37f5fa9864aebb5b5c194e
private1-cdn <143531144+private1-cdn@users.noreply.github.com>
2026-08-01 18:19:05 +0600

    Update json.js

--- Test-Video/json.js
+++ Test-Video/json.js
@@ -1,9 +1,290 @@
 
 export const video = [
   {
+    id: 40,
+    category: "Bangla",
+    title: "Favorite Video Number 40",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 39,
+    category: "Indian",
+    title: "Favorite Video Number 39",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 38,
+    category: "Bangla",
+    title: "Favorite Video Number 38",
+    ratio: "2",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 37,
+    category: "Indian",
+    title: "Favorite Video Number 37",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 36,
+    category: "Bangla",
+    title: "Favorite Video Number 36",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
+    id: 35,
+    category: "Bangla",
+    title: "Favorite Video Number 35",
+    ratio: "1",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 34,
+    category: "Indian",
+    title: "Favorite Video Number 34",
+    ratio: "2",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 33,
+    category: "Bangla",
+    title: "Favorite Video Number 33",
+    ratio: "1",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 32,
+    category: "Indian",
+    title: "Favorite Video Number 32",
+    ratio: "2",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 31,
+    category: "Bangla",
+    title: "Favorite Video Number 31",
+    ratio: "1",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
+    id: 30,
+    category: "Bangla",
+    title: "Favorite Video Number 30",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 29,
+    category: "Indian",
+    title: "Favorite Video Number 29",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 28,
+    category: "Bangla",
+    title: "Favorite Video Number 28",
+    ratio: "2",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 27,
+    category: "Indian",
+    title: "Favorite Video Number 27",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 26,
+    category: "Bangla",
+    title: "Favorite Video Number 26",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
+    id: 25,
+    category: "Bangla",
+    title: "Favorite Video Number 25",
+    ratio: "1",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 24,
+    category: "Indian",
+    title: "Favorite Video Number 24",
+    ratio: "2",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 23,
+    category: "Bangla",
+    title: "Favorite Video Number 23",
+    ratio: "1",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 22,
+    category: "Indian",
+    title: "Favorite Video Number 22",
+    ratio: "2",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 21,
+    category: "Bangla",
+    title: "Favorite Video Number 21",
+    ratio: "1",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
+    id: 20,
+    category: "Bangla",
+    title: "Favorite Video Number 20",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 19,
+    category: "Indian",
+    title: "Favorite Video Number 19",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 18,
+    category: "Bangla",
+    title: "Favorite Video Number 18",
+    ratio: "2",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 17,
+    category: "Indian",
+    title: "Favorite Video Number 17",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 16,
+    category: "Bangla",
+    title: "Favorite Video Number 16",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
+    id: 15,
+    category: "Bangla",
+    title: "Favorite Video Number 15",
+    ratio: "1",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 14,
+    category: "Indian",
+    title: "Favorite Video Number 14",
+    ratio: "2",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 13,
+    category: "Bangla",
+    title: "Favorite Video Number 13",
+    ratio: "1",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 12,
+    category: "Indian",
+    title: "Favorite Video Number 12",
+    ratio: "2",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 11,
+    category: "Bangla",
+    title: "Favorite Video Number 11",
+    ratio: "1",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
+    id: 10,
+    category: "Bangla",
+    title: "Favorite Video Number 10",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
+  },
+  {
+    id: 9,
+    category: "Indian",
+    title: "Favorite Video Number 9",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
+  },
+{
+    id: 8,
+    category: "Bangla",
+    title: "Favorite Video Number 8",
+    ratio: "2",
+    thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
+  },
+{
+    id: 7,
+    category: "Indian",
+    title: "Favorite Video Number 7",
+    ratio: "1",
+    thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
+  },
+  {
+    id: 6,
+    category: "Bangla",
+    title: "Favorite Video Number 6",
+    ratio: "2",
+    thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
+    source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
+  },
+  {
     id: 5,
     category: "Bangla",
     title: "Favorite Video Number 5",
+    ratio: "1",
     thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20250604/pngtree-lush-green-spring-nature-background-image_17387493.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/5.mp4"
   },
@@ -11,6 +292,7 @@ export const video = [
     id: 4,
     category: "Indian",
     title: "Favorite Video Number 4",
+    ratio: "2",
     thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/073/608/997/small/nature-background-nature-wallpaper-lush-green-meadow-with-a-large-tree-in-the-foreground-rolling-hills-and-dense-forest-in-the-background-free-photo.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/4.mp4"
   },
@@ -18,6 +300,7 @@ export const video = [
     id: 3,
     category: "Bangla",
     title: "Favorite Video Number 3",
+    ratio: "1",
     thumbnail: "https://pngmagic.com/webp_images/nature-based-youtube-thumbnail-background-in-hd_ECG7.webp",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/3.mp4"
   },
@@ -25,6 +308,7 @@ export const video = [
     id: 2,
     category: "Indian",
     title: "Favorite Video Number 2",
+    ratio: "2",
     thumbnail: "https://static.vecteezy.com/system/resources/thumbnails/024/264/951/small/blurred-spring-background-nature-with-blooming-glade-generative-ai-technology-free-photo.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/2.mp4"
   },
@@ -32,6 +316,7 @@ export const video = [
     id: 1,
     category: "Bangla",
     title: "Favorite Video Number 1",
+    ratio: "1",
     thumbnail: "https://png.pngtree.com/thumb_back/fh260/background/20230411/pngtree-nature-forest-sun-ecology-image_2256183.jpg",
     source: "https://raw.githubusercontent.com/private1-cdn/livetv-data/main/Test-Video/1.mp4"
   }
